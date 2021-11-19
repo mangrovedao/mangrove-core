6aeb8d65b37e49ab7c0ee0dd54a9c0ceb8f5d520
JeanKa <jean@mangrove.exchange>
2021-11-19 16:40:17 +0100

    script to post mumbai persistent offers

--- scripts/mumbai/startMumbaiPersistentOffer.js
+++ scripts/mumbai/startMumbaiPersistentOffer.js
@@ -1,33 +1,30 @@
 const hre = require("hardhat");
 const helper = require("../helper");
+const lc = require("../../lib/libcommon");
 
 async function main() {
-  const MumbaiMinter = await hre.ethers.getContract("MumbaiMinter");
-  const mgv = await helper.getMangrove();
-  const signerAddr = await MumbaiMinter.signer.getAddress();
-  const admin = await MumbaiMinter["admin()"]();
+  const repostLogic = await hre.ethers.getContract("Reposting");
+  const mgvContracts = await helper.getMangrove();
+  const mgv = mgvContracts.contract.connect(repostLogic.signer);
 
-  const wethAddr = helper.contractOfToken("wEth").address;
-  const daiAddr = helper.contractOfToken("dai").address;
-  const usdcAddr = helper.contractOfToken("usdc").address;
-
-  let overrides = { value: ethers.utils.parseEther("1.0") };
-  await mgv.contract["fund(address)"](MumbaiMinter.address, overrides);
-
-  // reading deploy oracle for the deployed network
-  const oracle = require(`../${hre.network.name}/activationOracle`);
+  const weth = helper.contractOfToken("wEth").connect(repostLogic.signer);
+  const dai = helper.contractOfToken("dai").connect(repostLogic.signer);
+  const usdc = helper.contractOfToken("usdc").connect(repostLogic.signer);
 
   const tokenParams = [
-    [wethAddr, "WETH", 18, ethers.utils.parseEther(oracle["WETH"].price)],
-    [daiAddr, "DAI", 18, ethers.utils.parseEther(oracle["DAI"].price)],
-    [usdcAddr, "USDC", 6, ethers.utils.parseEther(oracle["USDC"].price)],
+    [weth, "WETH", 18, ethers.utils.parseEther("1")],
+    [dai, "DAI", 18, ethers.utils.parseEther("0.0003")],
+    [usdc, "USDC", 6, ethers.utils.parseEther("0.0003")],
   ];
 
-  const ofr_gasreq = ethers.BigNumber.from(500000);
+  const ofr_gasreq = ethers.BigNumber.from(100000);
   const ofr_gasprice = ethers.BigNumber.from(0);
   const ofr_pivot = ethers.BigNumber.from(0);
 
-  const usdToNative = ethers.utils.parseEther(oracle["USDC"].price);
+  const usdToNative = ethers.utils.parseEther("0.0003");
+
+  let overrides = { value: ethers.utils.parseEther("1.0") };
+  await mgv["fund(address)"](repostLogic.address, overrides);
 
   for (const [
     outbound_tkn,
@@ -35,14 +32,14 @@ async function main() {
     outDecimals,
     outTknInMatic,
   ] of tokenParams) {
-    const tx = await MumbaiMinter.approveMangrove(
-      outbound_tkn,
+    const tx = await repostLogic.approveMangrove(
+      outbound_tkn.address,
       ethers.constants.MaxUint256
     );
     await tx.wait();
 
     for (const [inbound_tkn, inName, inDecimals, inTknInMatic] of tokenParams) {
-      if (outbound_tkn != inbound_tkn) {
+      if (outbound_tkn.address != inbound_tkn.address) {
         const makerWants = ethers.utils
           .parseUnits("1000", inDecimals)
           .mul(usdToNative)
@@ -52,9 +49,9 @@ async function main() {
           .mul(usdToNative)
           .div(outTknInMatic); // makerGives
 
-        const ofrTx = await MumbaiMinter.newOffer(
-          outbound_tkn, //e.g weth
-          inbound_tkn, //e.g dai
+        const ofrTx = await repostLogic.newOffer(
+          outbound_tkn.address, //e.g weth
+          inbound_tkn.address, //e.g dai
           makerWants,
           makerGives,
           ofr_gasreq,
@@ -62,18 +59,13 @@ async function main() {
           ofr_pivot
         );
         await ofrTx.wait();
-        const [, , offers] = await mgv.reader.offerList(
-          outbound_tkn,
-          inbound_tkn,
+        const book = await mgvContracts.reader.offerList(
+          outbound_tkn.address,
+          inbound_tkn.address,
           ethers.BigNumber.from(0),
           ethers.BigNumber.from(1)
         );
-        console.log(
-          `Out[${outName}]`,
-          `Inb[${inName}]`,
-          ethers.utils.formatUnits(offers[0].wants, inDecimals),
-          ethers.utils.formatUnits(offers[0].gives, outDecimals)
-        );
+        lc.logOrderBook(book, outbound_tkn, inbound_tkn);
       }
     }
   }

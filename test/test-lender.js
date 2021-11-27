af403ce8fb8f58884d83af2506b6bd99e0501552
JeanKa <jean@mangrove.exchange>
2021-11-27 15:39:58 +0100

    wip take profit

--- test/test-lender.js
+++ test/test-lender.js
@@ -1,15 +1,11 @@
 const { assert } = require("chai");
-//const { parseToken } = require("ethers/lib/utils");
-const { ethers, env, mangrove, network } = require("hardhat");
+const { ethers } = require("hardhat");
 const lc = require("lib/libcommon.js");
-const chalk = require("chalk");
-const { listenMgv, listenERC20 } = require("../lib/libcommon");
-// const config = require ("config");
-// const url = config.hardhat.networks.hardhat.forking.url;
-// const blockNumber = config.hardhat.networks.hardhat.forking.blockNumber;
+const { listenMgv, parseToken, listenERC20 } = require("../lib/libcommon");
+
+const { execLenderStrat, execTraderStrat } = require("./Exec/lenderStrats");
 
 let testSigner = null;
-const zero = lc.parseToken("0.0", 18);
 
 async function deployStrat(strategy, mgv) {
   const dai = (await lc.getContract("DAI")).connect(testSigner);
@@ -47,13 +43,13 @@ async function deployStrat(strategy, mgv) {
   await makerContract.deployed();
 
   // provisioning Mangrove on behalf of MakerContract
-  let overrides = { value: lc.parseToken("2.0", 18) };
+  let overrides = { value: parseToken("2.0", 18) };
   tx = await mgv["fund(address)"](makerContract.address, overrides);
   await tx.wait();
 
   lc.assertEqualBN(
     await mgv.balanceOf(makerContract.address),
-    lc.parseToken("2.0", 18),
+    parseToken("2.0", 18),
     "Failed to fund the Mangrove"
   );
 
@@ -89,7 +85,7 @@ async function deployStrat(strategy, mgv) {
   // One sends 1000 DAI to MakerContract
   mkrTxs[i++] = await dai.transfer(
     makerContract.address,
-    lc.parseToken("1000.0", await lc.getDecimals("DAI"))
+    parseToken("1000.0", await lc.getDecimals("DAI"))
   );
   // testSigner asks makerContract to approve lender to be able to mint [c/a]Token
   mkrTxs[i++] = await makerContract.approveLender(
@@ -103,7 +99,7 @@ async function deployStrat(strategy, mgv) {
   );
   // makerContract deposits some DAI on Lender (remains 100 DAIs on the contract)
   mkrTxs[i++] = await makerContract.mint(
-    lc.parseToken("900.0", await lc.getDecimals("DAI")),
+    parseToken("900.0", await lc.getDecimals("DAI")),
     market[1],
     makerContract.address
   );
@@ -111,53 +107,6 @@ async function deployStrat(strategy, mgv) {
   await lc.synch(mkrTxs);
 
   return makerContract;
-}
-
-async function execLenderStrat(makerContract, mgv, lenderName) {
-  const dai = await lc.getContract("DAI");
-  const wEth = await lc.getContract("WETH");
-  const gasreq = await makerContract.OFR_GASREQ();
-  //  listenERC20(wEth,18);
-
-  await lc.logLenderStatus(makerContract, lenderName, ["DAI", "WETH"]);
-
-  // // posting new offer on Mangrove via the MakerContract `newOffer` external function
-  let offerId = await lc.newOffer(
-    mgv,
-    makerContract,
-    "DAI", // outbound
-    "WETH", // inbound
-    lc.parseToken("0.5", await lc.getDecimals("WETH")), // required WETH
-    lc.parseToken("1000.0", await lc.getDecimals("DAI")) // promised DAI
-  );
-
-  let [takerGot, takerGave] = await lc.snipeSuccess(
-    mgv,
-    "DAI", // maker outbound
-    "WETH", // maker inbound
-    offerId,
-    lc.parseToken("800.0", await lc.getDecimals("DAI")), // taker wants 0.8 DAI
-    lc.parseToken("0.5", await lc.getDecimals("WETH")) // taker is ready to give up-to 0.5 WETH
-  );
-
-  lc.assertEqualBN(
-    takerGot,
-    lc.netOf(lc.parseToken("800.0", await lc.getDecimals("DAI")), fee),
-    "Incorrect received amount"
-  );
-
-  lc.assertEqualBN(
-    takerGave,
-    lc.parseToken("0.4", await lc.getDecimals("WETH")),
-    "Incorrect given amount"
-  );
-
-  // checking that MakerContract did put WETH on lender
-  await lc.expectAmountOnLender(makerContract, lenderName, [
-    ["DAI", lc.parseToken("200", await lc.getDecimals("DAI")), zero, 4],
-    ["WETH", takerGave, zero, 8],
-  ]);
-  await lc.logLenderStatus(makerContract, lenderName, ["DAI", "WETH"]);
 }
 
 /// start with 900 DAIs on lender and 100 DAIs locally
@@ -169,117 +118,6 @@ async function execLenderStrat(makerContract, mgv, lenderName) {
 /// now 1080 DAIs - locked DAI and 0 ETHs (borrower of 0.05 ETHs)
 /// newOffer: wants 0.63 ETHs for 1500 DAIs
 /// repays the full debt and borrows the missing part in DAI
-
-async function execTraderStrat(makerContract, mgv, lenderName) {
-  const dai = await lc.getContract("DAI");
-  const wEth = await lc.getContract("WETH");
-
-  await lc.logLenderStatus(makerContract, lenderName, ["DAI", "WETH"]);
-
-  // // posting new offer on Mangrove via the MakerContract `post` method
-  let offerId = await lc.newOffer(
-    mgv,
-    makerContract,
-    "DAI", //base
-    "WETH", //quote
-    lc.parseToken("0.15", await lc.getDecimals("WETH")), // required WETH
-    lc.parseToken("300.0", await lc.getDecimals("DAI")) // promised DAI (will need to borrow)
-  );
-
-  let [takerGot, takerGave] = await lc.snipeSuccess(
-    mgv,
-    "DAI", // maker base
-    "WETH", // maker quote
-    offerId,
-    lc.parseToken("300", await lc.getDecimals("DAI")),
-    lc.parseToken("0.15", await lc.getDecimals("WETH"))
-  );
-  lc.assertEqualBN(
-    takerGot,
-    lc.netOf(lc.parseToken("300.0", await lc.getDecimals("DAI")), fee),
-    "Incorrect received amount"
-  );
-  lc.assertEqualBN(
-    takerGave,
-    lc.parseToken("0.15", await lc.getDecimals("WETH")),
-    "Incorrect given amount"
-  );
-
-  await lc.logLenderStatus(makerContract, lenderName, ["DAI", "WETH"]);
-  await lc.expectAmountOnLender(makerContract, lenderName, [
-    ["DAI", lc.parseToken("700", await lc.getDecimals("DAI")), zero, 4],
-    ["WETH", takerGave, zero, 8],
-  ]);
-  // testSigner asks MakerContract to approve Mangrove for base (weth)
-  mkrTx2 = await makerContract
-    .connect(testSigner)
-    .approveMangrove(wEth.address, ethers.constants.MaxUint256);
-  await mkrTx2.wait();
-
-  offerId = await lc.newOffer(
-    mgv,
-    makerContract,
-    "WETH", // base
-    "DAI", //quote
-    lc.parseToken("380.0", await lc.getDecimals("DAI")), // wants DAI
-    lc.parseToken("0.2", await lc.getDecimals("WETH")) // promised WETH
-  );
-
-  [takerGot, takerGave] = await lc.snipeSuccess(
-    mgv,
-    "WETH",
-    "DAI",
-    offerId,
-    lc.parseToken("0.2", await lc.getDecimals("WETH")), // wanted WETH
-    lc.parseToken("380.0", await lc.getDecimals("DAI")) // giving DAI
-  );
-
-  lc.assertEqualBN(
-    takerGot,
-    lc.netOf(lc.parseToken("0.2", await lc.getDecimals("WETH")), fee),
-    "Incorrect received amount"
-  );
-  lc.assertEqualBN(
-    takerGave,
-    lc.parseToken("380", await lc.getDecimals("DAI")),
-    "Incorrect given amount"
-  );
-
-  await lc.logLenderStatus(makerContract, lenderName, ["DAI", "WETH"]);
-  await lc.expectAmountOnLender(makerContract, lenderName, [
-    // dai_on_lender = (1080 * CF_DAI * price_DAI - 0.05 * price_ETH)/price_DAI
-    ["WETH", zero, lc.parseToken("0.05", await lc.getDecimals("WETH")), 9],
-  ]);
-
-  offerId = await lc.newOffer(
-    mgv,
-    makerContract,
-    "DAI", //base
-    "WETH", //quote
-    lc.parseToken("0.63", await lc.getDecimals("WETH")), // wants ETH
-    lc.parseToken("1500", await lc.getDecimals("DAI")) // gives DAI
-  );
-  [takerGot, takerGave] = await lc.snipeSuccess(
-    mgv,
-    "DAI",
-    "WETH",
-    offerId,
-    lc.parseToken("1500", await lc.getDecimals("DAI")), // wanted DAI
-    lc.parseToken("0.63", await lc.getDecimals("WETH")) // giving WETH
-  );
-  lc.assertEqualBN(
-    takerGot,
-    lc.netOf(lc.parseToken("1500", await lc.getDecimals("DAI")), fee),
-    "Incorrect received amount"
-  );
-  lc.assertEqualBN(
-    takerGave,
-    lc.parseToken("0.63", await lc.getDecimals("WETH")),
-    "Incorrect given amount"
-  );
-  await lc.logLenderStatus(makerContract, lenderName, ["DAI", "WETH"]);
-  //TODO check borrowing DAIs and not borrowing WETHs anymore
-}
 
 describe("Deploy strategies", function () {
   this.timeout(200_000); // Deployment is slow so timeout is increased
@@ -304,12 +142,12 @@ describe("Deploy strategies", function () {
 
     lc.assertEqualBN(
       daiBal,
-      lc.parseToken("10000.0", await lc.getDecimals("DAI")),
+      parseToken("10000.0", await lc.getDecimals("DAI")),
       "Minting DAI failed"
     );
     lc.assertEqualBN(
       wethBal,
-      lc.parseToken("10.0", await lc.getDecimals("WETH")),
+      parseToken("10.0", await lc.getDecimals("WETH")),
       "Minting WETH failed"
     );
 

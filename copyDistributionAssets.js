6486008f97ac049bf178ccdeb6e9cfe67b509a63
Adrien Husson <adhusson@gmail.com>
2021-12-07 21:28:11 +0100

    fix distribution asset path

--- copyDistributionAssets.js
+++ copyDistributionAssets.js
@@ -1,13 +1,12 @@
 const shell = require("shelljs");
 const path = require("path");
-
 const distAbiDir = process.cwd() + "/dist/mangrove-abis/";
 shell.mkdir("-p", distAbiDir);
 console.log(path.join(distAbiDir, "*"));
 shell.rm("-rf", path.join(distAbiDir, "*"));
-shell.cd("build/exported-abis/"); // Workaround because shelljs.cp replicates the path to the files (contrary to regular `cp -R`)
+shell.cd("exported-abis/"); // Workaround because shelljs.cp replicates the path to the files (contrary to regular `cp -R`)
 shell.cp("-R", "./*", distAbiDir);
-shell.cd("../../");
+shell.cd("../");
 // adding true abi export for Mangroveoffer so mangrove.js has deploy code
 shell.cd(
   "artifacts/contracts/Strategies/OfferLogics/SingleUser/Deployable/SimpleMaker.sol"

82bc843daaf3b42052fa3a68f8edb0a71d0b4b25
Adrien Husson <adhusson@gmail.com>
2021-12-07 21:31:23 +0100

    increase copyDistributionAssets robustness
    - fail if a command errors
    - make upward cd not relative but absolute

--- copyDistributionAssets.js
+++ copyDistributionAssets.js
@@ -1,12 +1,13 @@
 const shell = require("shelljs");
 const path = require("path");
+shell.config.fatal = true; // throw if a command errors
+const here = shell.pwd();
 const distAbiDir = process.cwd() + "/dist/mangrove-abis/";
 shell.mkdir("-p", distAbiDir);
-console.log(path.join(distAbiDir, "*"));
 shell.rm("-rf", path.join(distAbiDir, "*"));
 shell.cd("exported-abis/"); // Workaround because shelljs.cp replicates the path to the files (contrary to regular `cp -R`)
 shell.cp("-R", "./*", distAbiDir);
-shell.cd("../");
+shell.cd(here);
 // adding true abi export for Mangroveoffer so mangrove.js has deploy code
 shell.cd(
   "artifacts/contracts/Strategies/OfferLogics/SingleUser/Deployable/SimpleMaker.sol"

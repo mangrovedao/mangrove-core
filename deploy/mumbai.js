801629779e243bfc134913b9c45b8e451e0a36d7
JeanKa <jean@mangrove.exchange>
2022-01-04 14:34:40 +0100

    New deployment naming convention `deployment` folder has the contract-vX.json history

--- deploy/mumbai.js
+++ deploy/mumbai.js
@@ -1,13 +1,15 @@
 module.exports = async (hre) => {
   const deployer = (await hre.getUnnamedAccounts())[0];
 
-  const mangroveResult = await hre.deployments.deploy("Mangrove-v1", {
+  const mangroveResult = await hre.deployments.deploy("Mangrove", {
+    contract: "Mangrove",
     from: deployer,
-    args: [deployer /* governance */, 40 /*gasprice*/, 500000 /*gasmax*/],
+    args: [deployer /* governance */, 40 /*gasprice*/, 700000 /*gasmax*/],
     skipIfAlreadyDeployed: true,
   });
 
-  const mgvReader = await hre.deployments.deploy("MgvReader-v1", {
+  const mgvReader = await hre.deployments.deploy("MgvReader", {
+    contract: "MgvReader",
     from: deployer,
     args: [mangroveResult.address],
     skipIfAlreadyDeployed: true,
@@ -25,14 +27,16 @@ module.exports = async (hre) => {
     skipIfAlreadyDeployed: true,
   });
 
-  const minter = await hre.deployments.deploy("Reposting", {
+  const reposting = await hre.deployments.deploy("Reposting", {
     from: deployer,
     args: [mangroveResult.address],
     skipIfAlreadyDeployed: true,
   });
 
+  // requires NODE_ENV=[network_name] to tell helper where to get the contract addresses
   const addressesProvider =
     require("../scripts/helper").getAave().addressesProvider;
+
   const offerProxy = await hre.deployments.deploy("OfferProxy", {
     from: deployer,
     args: [
@@ -40,20 +44,6 @@ module.exports = async (hre) => {
       mgvReader.address,
       mangroveResult.address,
     ],
-    skipIfAlreadyDeployed: true,
-  });
-
-  const mangroveResult2 = await hre.deployments.deploy("Mangrove", {
-    contract: "Mangrove",
-    from: deployer,
-    args: [deployer /* governance */, 40 /*gasprice*/, 700000 /*gasmax*/],
-    skipIfAlreadyDeployed: true,
-  });
-
-  const mgvReader2 = await hre.deployments.deploy("MgvReader", {
-    contract: "MgvReader",
-    from: deployer,
-    args: [mangroveResult2.address],
     skipIfAlreadyDeployed: true,
   });
 };

4383be10b73b53971c939934169a4e6d9d7ebfcd
JeanKa <jean@mangrove.exchange>
2022-02-18 12:48:20 +0100

    deployment info

--- scripts/mumbai/prodTests/MM/initializeMango.js
+++ scripts/mumbai/prodTests/MM/initializeMango.js
@@ -29,17 +29,27 @@ async function main() {
     ["DAI", "USDC"],
   ];
   for (const [baseName, quoteName] of markets) {
+    let tx = null;
     // NSLOTS/2 offers giving base (~1000 USD each)
     // NSLOTS/2 offers giving quote (~1000 USD)
 
     let MangoRaw = (
       await hre.ethers.getContract(`Mango_${baseName}_${quoteName}`)
     ).connect(deployer);
+
     if ((await MangoRaw.admin()) === deployer.address) {
-      const tx = await MangoRaw.setAdmin(tester.address);
+      tx = await MangoRaw.setAdmin(tester.address);
       await tx.wait();
     }
+
     MangoRaw = MangoRaw.connect(tester);
+    console.log(`* Set ${baseName} treasury to tester wallet`);
+    tx = await MangoRaw.set_treasury(true, tester.address);
+    await tx.wait();
+
+    console.log(`* Set ${quoteName} treasury to tester wallet`);
+    tx = await MangoRaw.set_treasury(false, tester.address);
+    await tx.wait();
 
     const NSLOTS = await MangoRaw.NSLOTS();
     const market = await MgvAPI.market({ base: baseName, quote: quoteName });
@@ -50,46 +60,30 @@ async function main() {
     const provAsk = await Mango.computeAskProvision();
     const totalFund = provAsk.add(provBid).mul(NSLOTS);
 
-    // This does not work as listener of maker contract is not synched with listener of Mgv events
-    const filter_initialized = MangoRaw.filters.Initialized();
-    let listened = 0;
-    MangoRaw.on(filter_initialized, async (from, to, bidding) => {
-      await market.requestBook(); //not helping
-      if (bidding) {
-        console.log(`=== Bids on market (${baseName},${quoteName}) ===`);
-        market.consoleBids();
-        listened++;
-      } else {
-        console.log(`=== Asks on market (${baseName},${quoteName}) ===`);
-        market.consoleAsks();
-        listened++;
-      }
-      if (listened == 2) {
-        MangoRaw.removeAllListeners();
-      }
-    });
-
     console.log(`* Funding mangrove (${totalFund} MATIC for Mango)`);
-    const tx = await Mango.fundMangrove(totalFund);
+    tx = await Mango.fundMangrove(totalFund);
     await tx.wait();
 
     console.log(
-      `* Approving mangrove for ${baseName} and ${quoteName} transfer`
+      `* Approving mangrove as spender for ${baseName} and ${quoteName} transfer from Mango`
     );
-    const tx1 = await Mango.approveMangroveForBase();
-    const tx2 = await Mango.approveMangroveForQuote();
+    let tx1 = await Mango.approveMangroveForBase();
+    let tx2 = await Mango.approveMangroveForQuote();
     await tx1.wait();
     await tx2.wait();
 
     console.log(
-      `* Approve Mango for ${baseName} and ${quoteName} token transfer`
+      `* Approve Mango as spender for ${baseName} and ${quoteName} token transfer from tester wallet`
     );
 
-    await MgvAPI.token(baseName).approve(MangoRaw.address);
-    await MgvAPI.token(quoteName).approve(MangoRaw.address);
+    tx1 = await MgvAPI.token(baseName).approve(MangoRaw.address);
+    tx2 = await MgvAPI.token(quoteName).approve(MangoRaw.address);
+    await tx1.wait();
+    await tx2.wait();
 
     console.log(`* Posting Mango offers on (${baseName},${quoteName}) market`);
-    const slice = NSLOTS / 2;
+    const chunks = 5;
+    const slice = NSLOTS / chunks;
     let bidding = true;
     let pivotIds = new Array(slice);
     let amounts = new Array(slice);
@@ -97,8 +91,8 @@ async function main() {
     pivotIds = pivotIds.fill(0, 0);
     amounts.fill(MgvAPI.toUnits(1000, quoteName), 0); // quotes are always in USD equivalent so using a volume of 1000 USD here
 
-    for (let i = 0; i < 2; i++) {
-      if (i >= 1) {
+    for (let i = 0; i < chunks; i++) {
+      if (i >= chunks / 2) {
         bidding = false;
       }
       const receipt = await MangoRaw.initialize(
@@ -110,9 +104,10 @@ async function main() {
         amounts
       );
       console.log(
-        `* Slice [${[slice * i, slice * (i + 1)]}[ initialized (${
-          (await receipt.wait()).gasUsed
-        } gas used)`
+        `* ${bidding ? "Bids" : "Asks"} [${[
+          slice * i,
+          slice * (i + 1),
+        ]}[ initialized (${(await receipt.wait()).gasUsed} gas used)`
       );
     }
   }

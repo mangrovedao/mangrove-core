4dc8317739885caf0fb374e0f079db3b158f9dd5
JeanKa <jean@mangrove.exchange>
2022-02-22 18:51:18 +0100

    oasisLike deploy script

---
+++ deploy/mumbai-OasisLike.js
@@ -0,0 +1,23 @@
+const { Mangrove } = require("../../mangrove.js");
+
+module.exports = async (hre) => {
+  const deployer = (await hre.getUnnamedAccounts())[0];
+  if (!deployer) {
+    throw Error("No deployer account found in the hardhat environment.");
+  }
+  const signer = await hre.ethers.getSigner(deployer);
+  const MgvAPI = await Mangrove.connect({
+    signer: signer,
+  });
+
+  const Oasis = await hre.deployments.deploy("OasisLike", {
+    from: deployer,
+    args: [MgvAPI.readerContract.address, MgvAPI.contract.address],
+    skipIfAlreadyDeployed: true,
+  });
+  console.log(
+    `OasisLike multi maker contract deployed (${Oasis.address}), don't forget to activate it.`
+  );
+};
+
+module.exports.tags = ["mumbai-OasisLike"];

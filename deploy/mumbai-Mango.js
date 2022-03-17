685bb03f9814c233a01f88e686f2b0db149e5aba
JeanKa <jean@mangrove.exchange>
2022-03-17 15:17:39 +0100

    Tighter deploy parameters

--- deploy/mumbai-Mango.js
+++ deploy/mumbai-Mango.js
@@ -23,7 +23,7 @@ module.exports = async (hre) => {
           MgvAPI.token(baseName).address, // base
           MgvAPI.token(quoteName).address, // quote
           // Pmin = QUOTE0/BASE0
-          MgvAPI.toUnits(base0, baseName),
+          MgvAPI.toUnits(base0, baseName), // BASE0
           MgvAPI.toUnits(quote0, quoteName), // QUOTE0
           NSLOTS, // price slots
           MgvAPI.toUnits(delta, quoteName), // quote progression
@@ -35,9 +35,9 @@ module.exports = async (hre) => {
       `Mango deployed (${Mango.address}) on market (${baseName},${quoteName})`
     );
   };
-  await deployOnMarket("WETH", "USDC", 0.5, 1000, 40); // [2000 USD/ETH, 4000 USD/ETH] inc 80 USD
-  await deployOnMarket("WETH", "DAI", 0.5, 1000, 40); // [2000 DAI/ETH, 4000 USD/ETH] inc 80 DAI
-  await deployOnMarket("DAI", "USDC", 1000, 997, 0.1); // [0.997 USD/DAI, 1.002 USD/DAI] in 1.E-4 USD
+  await deployOnMarket("WETH", "USDC", 1, 2000, 32); // [2000 USD/ETH,...,|2800|..., 3600 USD/ETH] inc 32 USD
+  await deployOnMarket("WETH", "DAI", 1, 2000, 32); // [2000 DAI/ETH,..,|2800|,... 3600 USD/ETH] inc 32 DAI
+  await deployOnMarket("DAI", "USDC", 1000, 997, 0.12); // Pmin=997/1000, inc=0.12/1000, Pmax= 997/1000 + 0.12*50/1000 = 1003/1000
 };
 
 module.exports.tags = ["mumbai-Mango"];

cbd7938882e7d19463a2931b792b71076c11c088
JeanKa <jean@mangrove.exchange>
2022-04-15 10:31:47 +0200

    fixing test

--- test/test-offerProxy.js
+++ test/test-offerProxy.js
@@ -172,7 +172,7 @@ describe("Deploy offerProxy", function () {
 
   it("Clean revert", async function () {
     // check that getFail is emitted during offer logic posthook
-    lc.listenOfferLogic(offerProxy, "MgvOffer/abort/getFailed");
+    lc.listenOfferLogic(offerProxy, await offerProxy.OUTOFLIQUIDITY());
     const aDai = await lc.getContract("ADAI");
     const dai = await lc.getContract("DAI");
     const wEth = await lc.getContract("WETH");

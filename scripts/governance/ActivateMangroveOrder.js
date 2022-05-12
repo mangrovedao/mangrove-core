4c8adc0cfcdd730e0735b3e6dec9f161741bd506
Espen Højsgaard <espen@espen.dk>
2022-05-12 14:47:44 +0200

    feat: Add MgvOracle configuration script
    As the only contract configuration, MgvOracle configuration was previously done via dapp tools - all other configuration is done via JS scripts.
    To avoid this inconsistency and related overhead, a configuration script for MgvOracle has been added to the set of governance scripts.

--- scripts/governance/ActivateMangroveOrder.js
+++ scripts/governance/ActivateMangroveOrder.js
@@ -5,7 +5,9 @@ const { Mangrove } = require("../../../mangrove.js");
 async function main() {
   const provider = ethers.getDefaultProvider(hre.network.config.url);
   if (!process.env["MUMBAI_DEPLOYER_PRIVATE_KEY"]) {
-    console.error("No tester account defined");
+    console.error(
+      "No deployer account defined, make sure MUMBAI_DEPLOYER_PRIVATE_KEY is set"
+    );
   }
 
   const wallet = new ethers.Wallet(

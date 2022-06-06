d3b724ee9a10ce25feb62db1aebe9053ff0a543d
JeanKa <jean@mangrove.exchange>
2022-06-06 18:24:56 +0200

    mint only for tests

--- contracts/Strategies/interfaces/IEIP20.sol
+++ contracts/Strategies/interfaces/IEIP20.sol
@@ -48,7 +48,4 @@ interface IEIP20 {
   function withdraw(uint) external;
 
   function decimals() external view returns (uint8);
-
-  // for mintable contracts
-  function mint(address, uint) external;
 }

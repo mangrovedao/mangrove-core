7315de9e098af6c69db407c41cc7ccd85945d728
Peter Kejlberg Madsen <peter@mangrove.exchange>
2022-09-23 13:22:15 +0200

    fix(format): added forge fmt to precommit

---
+++ lint-staged.config.js
@@ -0,0 +1,13 @@
+// lint-staged.config.js
+const micromatch = require("micromatch");
+
+module.exports = {
+  "*.sol": (files) => {
+    // from `files` filter those _NOT_ matching `*test.js`
+    const match = micromatch.not(files, "**/mangrove-solidity/lib/**");
+    const match1 = micromatch.not(match, "**/vendor/**");
+    const match2 = micromatch.not(match1, "**/preprocessed/**");
+    return `forge fmt ${match2.join(" ")}`;
+  },
+  "*.{js,css,md,json}": "prettier --write --ignore-unknown",
+};

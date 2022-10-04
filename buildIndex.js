66a97b0f635ded8bbd86e4245fee464b715decb9
Adrien Husson <adhusson@gmail.com>
2022-10-04 10:52:40 +0200

    chore: fixup build processes

---
+++ buildIndex.js
@@ -0,0 +1,31 @@
+/* ***** Mangrove tooling script ********* *
+This script dynamically creates an index.js file for the npm-dist directory of mangrove-core.
+
+For instance, if the `dist/mangrove-abis` directory contains `Mangrove.json` and
+`Maker.json`, it will include the following in `index.js`:
+
+    exports.abis = {};
+    exports.abis.Mangrove = require('mangrove-abis/Mangrove.json');
+    exports.abis.Maker = require('mangrove-abis/Maker.json');
+
+*/
+
+const fs = require("fs");
+
+const exportAllIn = (exportName,dir) => {
+  const lines = [];
+  lines.push(`exports.${exportName} = {};`);
+
+  for (const fileName of fs.readdirSync(`dist/${dir}`)) {
+    lines.push(`exports.${exportName} = require("./${dir}/${fileName}");`);
+  }
+  return lines.join('\n');
+}
+
+const indexLines = [];
+indexLines.push(exportAllIn('abis','mangrove-abis'));
+indexLines.push(exportAllIn('addresses','addresses/deployed'));
+
+fs.writeFileSync('dist/index.js',indexLines.join('\n'));
+
+console.log("Wrote index.js");

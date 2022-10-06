96d548880daf11ba531178e8418af27eebc17efc
Peter Kejlberg Madsen <peter@mangrove.exchange>
2022-10-06 16:33:12 +0200

    feat: added new GhostForwarder start + test

---
+++ src/toy_strategies/offer_forwarder/GhostForwarder.sol
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier:	BSD-2-Clause
+
+// Ghost.sol
+
+// Copyright (c) 2021 Giry SAS. All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+pragma solidity ^0.8.10;
+
+pragma abicoder v2;
+
+import "mgv_src/strategies/offer_forwarder/abstract/Forwarder.sol";
+import "mgv_src/strategies/routers/SimpleRouter.sol";
+import {MgvLib, MgvStructs} from "mgv_src/MgvLib.sol";
+
+contract GhostForwarder is Forwarder {
+  IERC20 public immutable BASE;
+  IERC20 public immutable STABLE1;
+  IERC20 public immutable STABLE2;
+
+  mapping(address => uint) offerId1; // mapping from maker address to id of the offer on stable 1
+  mapping(address => uint) offerId2; // mapping from maker address to id of the offer on stable 2
+
+  constructor(IMangrove mgv, IERC20 base, IERC20 stable1, IERC20 stable2, address deployer)
+    Forwarder(mgv, new SimpleRouter())
+  {
+    // SimpleRouter takes promised liquidity from admin's address (wallet)
+    STABLE1 = stable1;
+    STABLE2 = stable2;
+    BASE = base;
+
+    AbstractRouter router_ = router();
+    router_.bind(address(this));
+    if (deployer != msg.sender) {
+      setAdmin(deployer);
+      router_.setAdmin(deployer);
+    }
+  }
+
+  /**
+   * @param gives in BASE decimals
+   * @param wants1 in STABLE1 decimals
+   * @param wants2 in STABLE2 decimals
+   * @param pivot1 pivot for STABLE1
+   * @param pivot2 pivot for STABLE2
+   * @return (offerid for STABLE1, offerid for STABLE2)
+   * @dev these offer's provision must be in msg.value
+   * @dev `reserve()` must have approved base for `this` contract transfer prior to calling this function
+   */
+  function newGhostOffers(
+    // this function posts two asks
+    uint gives,
+    uint wants1,
+    uint wants2,
+    uint pivot1,
+    uint pivot2,
+    uint fund1,
+    uint fund2
+  ) external payable returns (uint, uint) {
+    // there is a cost of being paternalistic here, we read MGV storage
+    // an offer can be in 4 states:
+    // - not on mangrove (never has been)
+    // - on an offer list (isLive)
+    // - not on an offer list (!isLive) (and can be deprovisioned or not)
+    // MGV.retractOffer(..., deprovision:bool)
+    // deprovisioning an offer (via MGV.retractOffer) credits maker balance on Mangrove (no native token transfer)
+    // if maker wishes to retrieve native tokens it should call MGV.withdraw (and have a positive balance)
+    require(
+      !MGV.isLive(MGV.offers(address(BASE), address(STABLE1), offerId1[msg.sender])),
+      "GhostForwarder/offer1AlreadyActive"
+    );
+    require(
+      !MGV.isLive(MGV.offers(address(BASE), address(STABLE2), offerId2[msg.sender])),
+      "GhostForwarder/offer2AlreadyActive"
+    );
+    // FIXME the above requirements are not enough because offerId might be live on another base, stable market
+
+    uint _offerId1 = _newOffer(
+      NewOfferArgs({
+        outbound_tkn: BASE,
+        inbound_tkn: STABLE1,
+        wants: wants1,
+        gives: gives,
+        gasreq: offerGasreq(),
+        pivotId: pivot1,
+        caller: msg.sender,
+        fund: fund1,
+        noRevert: false
+      })
+    );
+
+    offerId1[msg.sender] = _offerId1;
+    // no need to fund this second call for provision
+    // since the above call should be enough
+    uint _offerId2 = _newOffer(
+      NewOfferArgs({
+        outbound_tkn: BASE,
+        inbound_tkn: STABLE2,
+        wants: wants2,
+        gives: gives,
+        gasreq: offerGasreq(),
+        pivotId: pivot2,
+        caller: msg.sender,
+        fund: fund2,
+        noRevert: false
+      })
+    );
+    offerId2[msg.sender] = _offerId2;
+
+    require(_offerId1 != 0, "GhostForwarder/newOffer1Failed");
+    require(_offerId2 != 0, "GhostForwarder/newOffer2Failed");
+
+    return (_offerId1, _offerId2);
+  }
+
+  function __posthookSuccess__(MgvLib.SingleOrder calldata order, bytes32 makerData)
+    internal
+    override
+    returns (bytes32)
+  {
+    // reposts residual if any (conservative hook)
+    bytes32 repost_status = super.__posthookSuccess__(order, makerData);
+    // write here what you want to do if not `reposted`
+    // reasons for not ok are:
+    // - residual below density (dust)
+    // - not enough provision
+    // - offer list is closed (governance call)
+    // Get the owner of the order. That is the same owner as the alt offer
+    address owner = ownerOf(IERC20(order.outbound_tkn), IERC20(order.inbound_tkn), order.offerId);
+    (IERC20 alt_stable, uint alt_offerId) =
+      IERC20(order.inbound_tkn) == STABLE1 ? (STABLE2, offerId2[owner]) : (STABLE1, offerId1[owner]);
+
+    if (repost_status == "posthook/reposted") {
+      uint new_alt_gives = __residualGives__(order); // in base units
+      MgvStructs.OfferPacked alt_offer = MGV.offers(order.outbound_tkn, address(alt_stable), alt_offerId);
+      uint old_alt_wants = alt_offer.wants();
+      // old_alt_gives is also old_gives
+      uint old_alt_gives = order.offer.gives();
+      // we want new_alt_wants == (old_alt_wants:96 * new_alt_gives:96)/old_alt_gives:96
+      // so no overflow to be expected :)
+      uint new_alt_wants;
+      unchecked {
+        new_alt_wants = (old_alt_wants * new_alt_gives) / old_alt_gives;
+      }
+
+      //uint prov = getMissingProvision(IERC20(order.outbound_tkn), IERC20(alt_stable), type(uint).max, 0, 0);
+
+      // the call below might throw
+      updateOffer({
+        outbound_tkn: IERC20(order.outbound_tkn),
+        inbound_tkn: IERC20(alt_stable),
+        wants: new_alt_wants,
+        gives: new_alt_gives,
+        gasreq: offerGasreq(),
+        gasprice: 0,
+        pivotId: alt_offer.next(),
+        offerId: alt_offerId
+      });
+      return "posthook/bothOfferReposted";
+    } else {
+      // repost failed or offer was entirely taken
+      retractOffer({
+        outbound_tkn: IERC20(order.outbound_tkn),
+        inbound_tkn: IERC20(order.inbound_tkn),
+        offerId: order.offerId,
+        deprovision: false
+      });
+      retractOffer({
+        outbound_tkn: IERC20(order.outbound_tkn),
+        inbound_tkn: IERC20(alt_stable),
+        offerId: alt_offerId,
+        deprovision: false
+      });
+      return "posthook/bothRetracted";
+    }
+  }
+
+  function __posthookFallback__(MgvLib.SingleOrder calldata order, MgvLib.OrderResult calldata)
+    internal
+    override
+    returns (bytes32)
+  {
+    // Get the owner of the order. That is the same owner as the alt offer
+    address owner = ownerOf(IERC20(order.outbound_tkn), IERC20(order.inbound_tkn), order.offerId);
+    // if we reach this code, trade has failed for lack of base token
+    (IERC20 alt_stable, uint alt_offerId) =
+      IERC20(order.inbound_tkn) == STABLE1 ? (STABLE2, offerId2[owner]) : (STABLE1, offerId1[owner]);
+    retractOffer({
+      outbound_tkn: IERC20(order.outbound_tkn),
+      inbound_tkn: IERC20(alt_stable),
+      offerId: alt_offerId,
+      deprovision: false
+    });
+    return "posthook/bothFailing";
+  }
+}

5ded05883064259ce08f8205648d7c44fb69b868
jkrivine <jean@mangrove.exchange>
2022-10-07 12:22:59 +0200

    updating copyrights claim

--- src/strategies/interfaces/IOfferLogic.sol
+++ src/strategies/interfaces/IOfferLogic.sol
@@ -2,7 +2,7 @@
 
 // IOfferLogic.sol
 
-// Copyright (c) 2021 Giry SAS. All rights reserved.
+// Copyright (c) 2022 ADDMA. All rights reserved.
 
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 

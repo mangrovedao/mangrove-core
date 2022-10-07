cf412bfb50fcad8a78fb68cd523ab0996455af0e
jkrivine <jean@mangrove.exchange>
2022-10-07 09:41:19 +0200

    more annotations

--- src/strategies/interfaces/IOfferLogic.sol
+++ src/strategies/interfaces/IOfferLogic.sol
@@ -14,12 +14,13 @@ pragma solidity >=0.8.0;
 
 pragma abicoder v2;
 
-import {IMangrove} from "mgv_src/IMangrove.sol";
-import {IERC20, IMaker} from "mgv_src/MgvLib.sol";
-import {AbstractRouter} from "mgv_src/strategies/routers/AbstractRouter.sol";
+import {IMangrove} from "src/IMangrove.sol";
+import {IERC20, IMaker} from "src/MgvLib.sol";
+import {AbstractRouter} from "src/strategies/routers/AbstractRouter.sol";
 
 ///@title IOfferLogic interface for offer management
-///@notice It is an IMaker for Mangrove
+///@notice It is an IMaker for Mangrove.
+/// In the annotation we use the term "offer maker" to refer to either `msg.sender` in `Forwarder` strats or `this`, for `Direct` strats.
 
 interface IOfferLogic is IMaker {
   ///@notice Log incident (during post trade execution)
@@ -38,11 +39,11 @@ interface IOfferLogic is IMaker {
   ///@notice Logging change in default gasreq
   event SetGasreq(uint);
 
-  ///@notice Actual gas requirement when posting offers via `this` strategy. Returned value may change if `this` contract's router is updated.
+  ///@notice Actual gas requirement when posting offers via this strategy. Returned value may change if this contract's router is updated.
   ///@return total gas cost including router specific costs (if any).
   function offerGasreq() external view returns (uint total);
 
-  ///@notice returns offer gas requirements.
+  ///@notice returns offer gas requirements, possibly disregarding router dependant cost.
   ///@param constantPartOnly whether one wishes to know the gas requirement w/o the router cost
   function detailedOfferGasreq(bool constantPartOnly) external view returns (uint);
 
@@ -53,40 +54,39 @@ interface IOfferLogic is IMaker {
     view
     returns (uint missingProvision);
 
-  ///@notice sets `this` contract's default gasreq for `new/updateOffer`.
+  ///@notice sets this contract's constant `gasreq` for `new/updateOffer`.
   ///@param gasreq an overapproximation of the gas required to handle trade and posthook without considering liquidity routing specific costs.
   ///@dev this should only take into account the gas cost of managing offer posting/updating during trade execution. Router specific gas cost are taken into account in the getter `offerGasreq()`
   function setGasreq(uint gasreq) external;
 
   ///@notice sets a new router to pull outbound tokens from contract's reserve to `this` and push inbound tokens to reserve.
   ///@param router_ the new router contract that this contract should use. Use `NO_ROUTER` for no router.
-  ///@dev new router needs to be approved by `this` contract to push funds to reserve (see `activate` function). It also needs to be approved by reserve to pull from it.
+  ///@dev new router needs to be approved by `this` to push funds to reserve (see `activate` function). It also needs to be approved by reserve to pull from it.
   function setRouter(AbstractRouter router_) external;
 
-  ///@notice Approves a spender to transfer a certain amount of tokens on behalf of `this` contract.
+  ///@notice Approves a spender to transfer a certain amount of tokens on behalf of `this`.
   ///@param token the ERC20 token contract
   ///@param spender the approved spender
   ///@param amount the spending amount
-  ///@dev admin may use this function to revoke approvals of `this` contract that are set after a call to `activate`.
+  ///@dev admin may use this function to revoke approvals of `this` that are set after a call to `activate`.
   function approve(IERC20 token, address spender, uint amount) external returns (bool);
 
-  ///@notice Withdraws tokens from offer maker's reserve
+  ///@notice Withdraws tokens from the offer maker's reserve
   ///@param token the type of asset one is willing to retrieve
   ///@param receiver the address of the receiver of the tokens (must not be `address(0)`)
   ///@param amount the quantity of tokens to withdraw from reserve (in WEI units).
   ///@return success whether funds were successfully transferred to `receiver`
-  ///@dev notice anyone can call but only `msg.sender` can withdraw from its reserve
   function withdrawToken(IERC20 token, address receiver, uint amount) external returns (bool success);
 
-  ///@notice computes the amount of native tokens that can be redeemed when deprovisioning a certain offer.
+  ///@notice computes the amount of native tokens that can be redeemed when deprovisioning a given offer.
   ///@param outbound_tkn the outbound token of the offer list
   ///@param inbound_tkn the inbound token of the offer list
   ///@param offerId the identifier of the offer in the offer list
   ///@return provision the amount of native tokens that can be redeemed when deprovisioning the offer
   function provisionOf(IERC20 outbound_tkn, IERC20 inbound_tkn, uint offerId) external view returns (uint provision);
 
-  ///@notice verifies that this contract's current state is ready to be used by msg.sender to post offers on Mangrove
-  ///@dev throws with a reason when there is a missing approval
+  ///@notice verifies that this contract's current state is ready to be used by `msg.sender` to post offers on Mangrove
+  ///@dev throws with a reason if something (e.g. an approval) is missing.
   function checkList(IERC20[] calldata tokens) external view;
 
   ///@notice View of offer maker's reserve balance for a particular asset.
@@ -94,11 +94,11 @@ interface IOfferLogic is IMaker {
   ///@return balance the `token` amount in offer maker's reserve
   function tokenBalance(IERC20 token) external view returns (uint balance);
 
-  /// @notice allows `this` contract to be a liquidity provider for a particular asset by performing the necessary approvals
-  /// @param tokens the ERC20 `this` contract will approve to be able to trade on Mangrove's corresponding markets.
+  /// @notice performs the required approvals so as to allow `this` to interact with Mangrove on a set of assets.
+  /// @param tokens the ERC20 `this` will approve to be able to trade on Mangrove's corresponding markets.
   function activate(IERC20[] calldata tokens) external;
 
-  ///@notice withdraws ETH from the `this` contract's balance on Mangrove.
+  ///@notice withdraws native tokens from `this` balance on Mangrove.
   ///@param amount the amount of WEI one wishes to withdraw.
   ///@param receiver the address of the receiver of the funds.
   ///@dev Since a call is made to the `receiver`, this function is subject to reentrancy.
@@ -137,7 +137,7 @@ interface IOfferLogic is IMaker {
     IERC20 outbound_tkn,
     IERC20 inbound_tkn,
     uint offerId,
-    bool deprovision // if set to `true`, `this` contract will receive the remaining provision (in WEI) associated to `offerId`.
+    bool deprovision // if set to `true`, `this` will receive the remaining provision (in WEI) associated to `offerId`.
   ) external returns (uint received);
 
   ///@notice view of offer maker's reserve

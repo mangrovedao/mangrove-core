aa03c4fbeba048bccc867dfca445b0f35c60bee4
jkrivine <jean@mangrove.exchange>
2022-10-15 12:30:50 +0200

    AbstractRouter coverage

--- test/strategies/unit/SimpleRouter.t.sol
+++ test/strategies/unit/SimpleRouter.t.sol
@@ -5,12 +5,26 @@ import "./OfferLogic.t.sol";
 import "src/strategies/routers/SimpleRouter.sol";
 
 contract SimpleRouterTest is OfferLogicTest {
+  SimpleRouter router;
+
   function setupLiquidityRouting() internal override {
-    vm.startPrank(maker);
-    SimpleRouter router = new SimpleRouter();
+    vm.startPrank(deployer);
+    router = new SimpleRouter();
     router.bind(address(makerContract));
-    makerContract.setReserve(maker, address(router));
     makerContract.setRouter(router);
+    makerContract.setReserve(maker, address(router));
     vm.stopPrank();
+  }
+
+  function test_admin_can_unbind_makerContract() public {
+    vm.prank(deployer);
+    router.unbind(address(makerContract));
+    assertTrue(!router.makers(address(makerContract)), "unbind failed");
+  }
+
+  function test_makerContract_can_unbind_makerContract() public {
+    vm.prank(address(makerContract));
+    router.unbind();
+    assertTrue(!router.makers(address(makerContract)), "unbind failed");
   }
 }

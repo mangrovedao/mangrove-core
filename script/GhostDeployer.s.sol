bfd2937658379acfd935744c23c289d99acf72eb
Peter <105609241+peterMangrove@users.noreply.github.com>
2022-10-20 14:30:21 +0200

    fix(GhostDeployer): better comments for ghost deployer (#43)

--- script/GhostDeployer.s.sol
+++ script/GhostDeployer.s.sol
@@ -2,15 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Ghost, AbstractRouter} from "mgv_src/toy_strategies/offer_maker/Ghost.sol";
-import {MangroveOrderEnriched, IERC20, IMangrove} from "mgv_src/periphery/MangroveOrderEnriched.sol";
+import {Ghost, AbstractRouter, IERC20, IMangrove} from "mgv_src/toy_strategies/offer_maker/Ghost.sol";
 import {Deployer} from "./lib/Deployer.sol";
 
 /*  Deploys a Ghost instance
     First test:
- ADMIN=$MUMBAI_PRIVATE_ADDRESS forge script --fork-url mumbai GhostDeployer -vvv
+ ADMIN=$MUMBAI_PUBLIC_KEY forge script --fork-url mumbai GhostDeployer -vvv
     Then broadcast and verify:
- ADMIN=$MUMBAI_PRIVATE_ADDRESS WRITE_DEPLOY=true forge script --fork-url mumbai GhostDeployer -vvv --broadcast --verify
+ ADMIN=$MUMBAI_PUBLIC_KEY WRITE_DEPLOY=true forge script --fork-url mumbai GhostDeployer -vvv --broadcast --verify
     Remember to activate it using Activate
 */
 contract GhostDeployer is Deployer {
@@ -24,7 +23,10 @@ contract GhostDeployer is Deployer {
   }
 
   /**
-   * @param admin address of the admin on MangroveOrder after deployment
+   * @param admin address of the admin on Ghost after deployment
+   * @param base address of the base on Ghost after deployment
+   * @param stable1 address of the first stable coin on Ghost after deployment
+   * @param stable2 address of the second stable coin on Ghost after deployment
    */
   function innerRun(address admin, address base, address stable1, address stable2) public {
     IMangrove mgv = IMangrove(fork.get("Mangrove"));
@@ -35,12 +37,12 @@ contract GhostDeployer is Deployer {
       if (bal > 0) {
         broadcast();
         old_ghost.withdrawFromMangrove(bal, payable(admin));
-        uint old_balance = old_ghost.admin().balance;
-        broadcast();
-        old_ghost.retractOffers(true);
-        uint new_balance = old_ghost.admin().balance;
-        console.log("Retrieved ", new_balance - old_balance + bal, "WEIs from old deployment", address(old_ghost));
       }
+      uint old_balance = old_ghost.admin().balance;
+      broadcast();
+      old_ghost.retractOffers(true);
+      uint new_balance = old_ghost.admin().balance;
+      console.log("Retrieved ", new_balance - old_balance + bal, "WEIs from old deployment", address(old_ghost));
     } catch {
       console.log("No existing Ghost in ToyENS");
     }

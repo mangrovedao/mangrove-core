2cf6d153ee65f7a67b073cf4102343a86183bbe9
JeanKa <jean@mangrove.exchange>
2022-10-31 10:31:22 +0100

    explicit import statements (#58)

--- src/strategies/offer_maker/OfferMaker.sol
+++ src/strategies/offer_maker/OfferMaker.sol
@@ -13,9 +13,8 @@ pragma solidity ^0.8.10;
 
 pragma abicoder v2;
 
-import "src/strategies/offer_maker/abstract/Direct.sol";
-import "src/strategies/routers/AbstractRouter.sol";
-import "src/strategies/interfaces/IMakerLogic.sol";
+import {Direct, AbstractRouter, IMangrove, IERC20} from "src/strategies/offer_maker/abstract/Direct.sol";
+import {IMakerLogic} from "src/strategies/interfaces/IMakerLogic.sol";
 
 contract OfferMaker is IMakerLogic, Direct {
   // router_ needs to bind to this contract

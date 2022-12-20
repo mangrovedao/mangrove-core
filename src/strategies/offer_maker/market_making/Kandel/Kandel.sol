e51ed4108475b3c18e35b8f941795eb09140bf42
jkrivine <jean@mangrove.exchange>
2022-12-20 18:27:25 +0100

    natspec + implementing populating Mangrove's order book

--- src/strategies/offer_maker/market_making/Kandel/Kandel.sol
+++ src/strategies/offer_maker/market_making/Kandel/Kandel.sol
@@ -17,11 +17,21 @@ import {MgvLib, MgvStructs} from "mgv_src/MgvLib.sol";
 contract Kandel is Direct {
   ///@notice number of price offers managed by this strat
   uint16 immutable NSLOTS;
+  ///@notice base of the market Kandel is making
   IERC20 immutable BASE;
+  ///@notice quote of the market Kandel is making
   IERC20 immutable QUOTE;
-
+  ///@notice quote distribution: `quoteOfIndex[i]` is the amount of quote tokens Kandel must give or want at index i
   uint[] quoteOfIndex;
+  ///@notice quote distribution: `baseOfIndex[i]` is the amount of base tokens Kandel must give or want at index i
   uint[] baseOfIndex;
+  ///@notice `pendingBase/Quote` that failed to be published and must be recycled when possible
+  uint pendingBase;
+  uint pendingQuote;
+
+  ///@notice signals that the price has move beyond Kandel's current price range
+  event AllAsks(IMangrove indexed mgv, IERC20 indexed base, IERC20 indexed quote);
+  event AllBids(IMangrove indexed mgv, IERC20 indexed base, IERC20 indexed quote);
 
   ///@notice a bid or an ask
   enum OrderType {
@@ -38,35 +48,67 @@ contract Kandel is Direct {
     QUOTE = quote;
   }
 
-  function givesOfIndex(OrderType ba, uint index) internal view returns (uint) {
+  ///@notice sets the base/quote distribution that Kandel should follow
+  ///@param inQuotes whether this call is setting quote distribution
+  ///@param from start index (included). Must be less than `to`.
+  ///@param to end index (excluded). Must be less than `NSLOT`.
+  ///@param slice the distrbution of base/quote in the interval [from, to[
+  function setDistribution(bool inQuotes, uint from, uint to, uint[] calldata slice) external onlyAdmin {
+    uint[] storage dist = inQuotes ? quoteOfIndex : baseOfIndex;
+    uint cpt = 0;
+    for (uint i = from; i < to; i++) {
+      dist[i] = slice[cpt];
+      cpt++;
+    }
+  }
+
+  ///@notice how much price and volume distribution says Kandel should give at given index
+  ///@param ba whether Kandel is asking or bidding at this index
+  ///@param index the distribution index
+  function _givesOfIndex(OrderType ba, uint index) internal view returns (uint) {
     return ba == OrderType.Ask ? quoteOfIndex[index] : baseOfIndex[index];
   }
 
-  function wantsOfIndex(OrderType ba, uint index) internal view returns (uint) {
+  ///@notice how much price and volume distribution says Kandel should want at given index
+  ///@param ba whether Kandel is asking or bidding at this index
+  ///@param index the distribution index
+  function _wantsOfIndex(OrderType ba, uint index) internal view returns (uint) {
     return ba == OrderType.Ask ? baseOfIndex[index] : quoteOfIndex[index];
   }
 
-  function tokenPairOfOrderType(OrderType ba) internal view returns (IERC20, IERC20) {
+  ///@notice turns an order type into an (outbound, inbound) pair identifying an offer list
+  ///@param ba whether one wishes to acces the offer lists where asks or bids are posted
+  function _tokenPairOfOrderType(OrderType ba) internal view returns (IERC20, IERC20) {
     return ba == OrderType.Bid ? (QUOTE, BASE) : (BASE, QUOTE);
   }
 
-  function orderTypeOfOutbound(IERC20 outbound_tkn) internal view returns (OrderType) {
+  ///@notice returns the Kandel order type of the offer list whose outbound token is given in argument
+  ///@param outbound_tkn the outbound token of the offer list
+  function _orderTypeOfOutbound(IERC20 outbound_tkn) internal view returns (OrderType) {
     return outbound_tkn == BASE ? OrderType.Ask : OrderType.Bid;
   }
 
+  ///@notice retrieve offer data on Mangrove
+  ///@param ba whether the offer is a Bid or an Ask
+  ///@param index the distribution index of the offer
   function getOffer(OrderType ba, uint index)
-    internal
+    public
     view
     returns (MgvStructs.OfferPacked, MgvStructs.OfferDetailPacked)
   {
-    (IERC20 outbound_tkn, IERC20 inbound_tkn) = tokenPairOfOrderType(ba);
+    (IERC20 outbound_tkn, IERC20 inbound_tkn) = _tokenPairOfOrderType(ba);
     return (
       MGV.offers(address(outbound_tkn), address(inbound_tkn), offerIdOfIndex[ba][index]),
       MGV.offerDetails(address(outbound_tkn), address(inbound_tkn), offerIdOfIndex[ba][index])
     );
   }
 
-  function postOrder(OrderType ba, uint index, OfferArgs memory args) internal returns (bytes32) {
+  ///@notice publishes (by either creating or updating) a bid/ask at a given price index
+  ///@param ba whether the offer is a bid or an ask
+  ///@param index the index of the distribution
+  ///@param args the argument of the offer.
+  ///@dev args.wants/gives must match the distribution at index
+  function _populateIndex(OrderType ba, uint index, OfferArgs memory args) internal returns (bytes32) {
     uint offerId = offerIdOfIndex[ba][index];
     if (offerId == 0) {
       offerId = _newOffer(args);
@@ -81,23 +123,59 @@ contract Kandel is Direct {
     }
   }
 
-  function getDualOrderArgs(OrderType ba, MgvLib.SingleOrder calldata order)
+  function populate(uint from, uint to, uint lastBidIndex, uint gasprice) external payable onlyAdmin {
+    if (msg.value > 0) {
+      MGV.fund{value: msg.value}();
+    }
+    for (uint index = from; index < to; index++) {
+      OfferArgs memory args;
+      OrderType ba = index <= lastBidIndex ? OrderType.Bid : OrderType.Ask;
+      (args.outbound_tkn, args.inbound_tkn) = _tokenPairOfOrderType(ba);
+      (args.gives, args.wants) =
+        ba == OrderType.Bid ? (quoteOfIndex[index], baseOfIndex[index]) : (baseOfIndex[index], quoteOfIndex[index]);
+      args.fund = 0;
+      args.noRevert = true;
+      args.gasreq = offerGasreq();
+      args.gasprice = gasprice;
+      _populateIndex(ba, index, args);
+    }
+  }
+
+  ///@notice hooks that implements transport logic
+  ///@param ba whether the offer that was executed is a bid or an ask
+  ///@param order a recap of the taker order (order.offer is the executed offer)
+  ///@return dualBa the type of order implementing the transport
+  ///@return dualIndex the distribution index where liquidity is transported
+  ///@return args the argument for `populateIndex` specifying volume and price
+  function __transportLogic__(OrderType ba, MgvLib.SingleOrder calldata order)
     internal
-    view
+    virtual
     returns (OrderType dualBa, uint dualIndex, OfferArgs memory args)
   {
     uint index = offerIdOfIndex[ba][order.offerId];
+    if (index == 0) {
+      emit AllAsks(MGV, BASE, QUOTE);
+    }
+    if (index == NSLOTS - 1) {
+      emit AllBids(MGV, BASE, QUOTE);
+    }
     (MgvStructs.OfferPacked dualOffer, MgvStructs.OfferDetailPacked dualOfferDetails) = getOffer(dualBa, dualIndex);
 
     (dualIndex, dualBa) = ba == OrderType.Ask ? (index - 1, OrderType.Bid) : (index + 1, OrderType.Ask);
 
-    uint maxDualGives = dualOffer.gives() + order.gives;
-    uint shouldGive = givesOfIndex(dualBa, dualIndex);
-    uint shouldWant = wantsOfIndex(dualBa, dualIndex);
+    // can repost what the current taker order gives + what the dual offer already gives.
+    uint maxDualGives = dualBa == OrderType.Ask
+      ? dualOffer.gives() + order.gives + pendingBase
+      : dualOffer.gives() + order.gives + pendingQuote;
+
+    // what the distribution says the dual order should ask/bid
+    uint shouldGive = _givesOfIndex(dualBa, dualIndex);
+    uint shouldWant = _wantsOfIndex(dualBa, dualIndex);
 
     args.outbound_tkn = IERC20(order.inbound_tkn);
     args.inbound_tkn = IERC20(order.outbound_tkn);
     args.gives = shouldGive > maxDualGives ? maxDualGives : shouldGive;
+    // if giving less volume than distribution, one must adapts wants to match distribution price
     args.wants = args.gives == shouldGive ? shouldWant : (maxDualGives * shouldWant) / shouldGive;
     args.fund = 0;
     args.noRevert = true;
@@ -105,24 +183,38 @@ contract Kandel is Direct {
     args.gasprice = dualOfferDetails.gasprice();
   }
 
+  ///@notice takes care of reposting residual offer in case of a partial fill and logging potential issues.
+  ///@param order a recap of the taker order
+  ///@param ba whether the executer offer (order.offer) is a bid or an ask
+  function _handleResidual(MgvLib.SingleOrder calldata order, bytes32 makerData, OrderType ba) internal {
+    bytes32 repostStatus = super.__posthookSuccess__(order, makerData);
+    // Offer failed to repost for bad reason, logging the incident
+    if (repostStatus == "posthook/filled" || repostStatus == REPOST_SUCCESS) {
+      return;
+    }
+    if (repostStatus == "mgv/writeOffer/density/tooLow") {
+      uint givesBelowDensity = __residualGives__(order);
+      if (ba == OrderType.Ask) {
+        pendingBase += givesBelowDensity;
+      } else {
+        pendingQuote += givesBelowDensity;
+      }
+    } else {
+      emit LogIncident(
+        MGV, IERC20(order.outbound_tkn), IERC20(order.inbound_tkn), order.offerId, makerData, repostStatus
+        );
+    }
+  }
+
   function __posthookSuccess__(MgvLib.SingleOrder calldata order, bytes32 makerData)
     internal
     override
     returns (bytes32)
   {
-    bytes32 repostStatus = super.__posthookSuccess__(order, makerData);
-    // Offer failed to repost for bad reason, loggin incident
-    if (
-      repostStatus != "posthook/filled" || repostStatus != REPOST_SUCCESS
-        || repostStatus != "mgv/writeOffer/density/tooLow"
-    ) {
-      emit LogIncident(
-        MGV, IERC20(order.outbound_tkn), IERC20(order.inbound_tkn), order.offerId, makerData, repostStatus
-        );
-    }
-    OrderType ba = orderTypeOfOutbound(IERC20(order.outbound_tkn));
+    OrderType ba = _orderTypeOfOutbound(IERC20(order.outbound_tkn));
+    _handleResidual(order, makerData, ba);
     // preparing arguments for the dual maker order
-    (OrderType ba_, uint index_, OfferArgs memory args) = getDualOrderArgs(ba, order);
-    return postOrder(ba_, index_, args);
+    (OrderType ba_, uint index_, OfferArgs memory args) = __transportLogic__(ba, order);
+    return _populateIndex(ba_, index_, args);
   }
 }

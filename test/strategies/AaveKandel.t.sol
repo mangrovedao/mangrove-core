6019a3925ee5a58ed0e8bc8d830f1c515e6b1e73
jkrivine <jean@mangrove.exchange>
2023-02-02 22:39:20 +0100

    tests for Aave kandel

--- test/strategies/AaveKandel.t.sol
+++ test/strategies/AaveKandel.t.sol
@@ -7,20 +7,39 @@ import {AaveKandel, AavePooledRouter} from "mgv_src/strategies/offer_maker/marke
 import {PinnedPolygonFork} from "mgv_test/lib/forks/Polygon.sol";
 
 contract AaveKandelTest is CoreKandelTest {
-  function deployKandel() internal virtual override returns (CoreKandel kdl_) {
+  PinnedPolygonFork fork;
+
+  function __setForkEnvironment__() internal override {
+    fork = new PinnedPolygonFork();
+    fork.setUp();
+    mgv = setupMangrove();
+    reader = new MgvReader($(mgv));
+    base = TestToken(fork.get("WETH"));
+    quote = TestToken(fork.get("USDC"));
+    setupMarket(base, quote);
+  }
+
+  function __deployKandel__(address deployer) internal virtual override returns (CoreKandel) {
+    // 474_000 theoretical in mock up of router
+    // 218_000 observed in tests of router
+    uint router_gasreq = 318 * 1000;
+    uint kandel_gasreq = 128 * 1000;
+    AavePooledRouter router = new AavePooledRouter(fork.get("Aave"), router_gasreq);
     HasIndexedOffers.MangroveWithBaseQuote memory mangroveWithBaseQuote =
-      HasIndexedOffers.MangroveWithBaseQuote({mgv: IMangrove($(mgv)), base: weth, quote: usdc});
-    vm.prank(maker);
-    kdl_ = new AaveKandel({
+      HasIndexedOffers.MangroveWithBaseQuote({mgv: IMangrove($(mgv)), base: base, quote: quote});
+
+    AaveKandel aaveKandel = new AaveKandel({
       mangroveWithBaseQuote: mangroveWithBaseQuote,
-      gasreq: GASREQ,
-      gasprice: bufferedGasprice,
-      owner: maker
+      gasreq: kandel_gasreq,
+      gasprice: 0,
+      owner: deployer
     });
 
-    router.bind(address(aaveKdl));
+    router.bind(address(aaveKandel));
     // Setting AaveRouter as Kandel's router and activating router on BASE and QUOTE ERC20
-    aaveKdl.initialize(router);
-    return aaveKdl;
+    aaveKandel.initialize(router);
+    aaveKandel.setAdmin(deployer);
+    assertEq(aaveKandel.offerGasreq(), kandel_gasreq + router_gasreq, "Incorrect gasreq");
+    return aaveKandel;
   }
 }

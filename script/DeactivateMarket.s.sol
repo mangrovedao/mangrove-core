1e2e009c0cff441c22e5a9e85971a067ec0cd0f5
Adrien Husson <adhusson@gmail.com>
2023-02-17 14:29:09 +0100

    make script reuse updatemarket, add tests, consistency

--- script/DeactivateMarket.s.sol
+++ script/DeactivateMarket.s.sol
@@ -2,37 +2,35 @@
 pragma solidity ^0.8.13;
 
 import {Deployer} from "mgv_script/lib/Deployer.sol";
-import {MgvOracle} from "mgv_src/periphery/MgvOracle.sol";
-import {IMangrove} from "mgv_src/IMangrove.sol";
+import {Mangrove} from "mgv_src/Mangrove.sol";
 import {MgvReader} from "mgv_src/periphery/MgvReader.sol";
-import {MgvStructs} from "mgv_src/MgvLib.sol";
-import {ERC20} from "mgv_src/toy/ERC20.sol";
+import {UpdateMarket} from "mgv_script/periphery/UpdateMarket.s.sol";
 
+/* Deactivate a market (aka two mangrove semibooks) & update MgvReader. */
 contract DeactivateMarket is Deployer {
   function run() public {
-    innerRun({tkn1: envAddressOrName("TKN1"), tkn2: envAddressOrName("TKN2")});
+    innerRun({tkn0: envAddressOrName("TKN0"), tkn1: envAddressOrName("TKN1")});
+    outputDeployment();
   }
 
-  function innerRun(address tkn1, address tkn2) public {
-    IMangrove mgv = IMangrove(fork.get("Mangrove"));
-    MgvReader mgvr = MgvReader(fork.get("MgvReader"));
-    require(mgv.governance() == broadcaster(), "Only governance can call");
-    broadcast();
-    mgv.deactivate(tkn1, tkn2);
+  function innerRun(address tkn0, address tkn1) public {
+    Mangrove mgv = Mangrove(fork.get("Mangrove"));
+    MgvReader reader = MgvReader(fork.get("MgvReader"));
 
     broadcast();
-    mgv.deactivate(tkn2, tkn1);
+    mgv.deactivate(tkn0, tkn1);
 
     broadcast();
-    mgvr.updateMarket(tkn1, tkn2);
+    mgv.deactivate(tkn1, tkn0);
 
-    smokeTest(mgv, mgvr, tkn1, tkn2);
+    (new UpdateMarket()).innerRun({tkn0: tkn0, tkn1: tkn1, mgvReaderAddress: address(reader)});
+
+    smokeTest(reader, tkn0, tkn1);
   }
 
-  function smokeTest(IMangrove mgv, MgvReader mgvr, address tkn1, address tkn2) internal view {
-    (, MgvStructs.LocalPacked local12) = mgv.config(tkn1, tkn2);
-    (, MgvStructs.LocalPacked local21) = mgv.config(tkn2, tkn1);
-    require(!(local12.active() || local21.active()), "Market was not deactivated");
-    require(!mgvr.isMarketOpen(tkn1, tkn2), "Reader state not updated");
+  function smokeTest(MgvReader reader, address tkn0, address tkn1) internal view {
+    MgvReader.MarketConfig memory config = reader.marketConfig(tkn0, tkn1);
+    require(!(config.config01.active || config.config10.active), "Market was not deactivated");
+    require(!reader.isMarketOpen(tkn0, tkn1), "Reader state not updated");
   }
 }

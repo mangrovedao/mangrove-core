21e9ed6ab5e5ac8152a5bda3c8c05c96a3d17bc4
Adrien Husson <adhusson@gmail.com>
2023-02-24 18:16:15 +0100

    doc: explain GetTokenDealSlot

--- script/lib/GetTokenDealSlot.sol
+++ script/lib/GetTokenDealSlot.sol
@@ -12,6 +12,26 @@ interface SmallERC20 {
 contract GetTokenDealSlot is Deployer {
   using stdStorage for StdStorage;
 
+  /* Read the storage slot used by a token to remember an account's balance.
+  Uses stdstore to do the following:
+  1. Record storage accesses.
+  2. Call token.balanceOf(account) (signature of balanceOf: 0x70a08231) .
+  3. Log the slot that was read during the call to balanceOf.
+  4. For convenience, also log the number of decimals used by the token.
+
+  The script is intended to be used as follows:
+  1. Given an anvil node running (with --fork-url <SOME_NODE>)
+  2. Run
+
+      forge script GetTokenDealSlot -vv
+
+  3. Parse output for slot: <slot>
+  4. Run
+
+      cast rpc anvil_setStorageAt <token> <slot> <amount>
+      (or directly do a json-rpc request from e.g. javascript)
+
+  */
   function run() public {
     address token = envAddressOrName("TOKEN");
     address account = envAddressOrName("ACCOUNT");

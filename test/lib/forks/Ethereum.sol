a7188c551a528bfe8a88996075788dffe187bc1a
0xAurelou <63618058+0xAurelou@users.noreply.github.com>
2023-04-01 13:02:59 +0200

    feat: add ethereum fork support (#284)
    fix: Ethereum chaind ID
    
    fix: Contract name fix
    
    chore: signed commit#2
    
    Signed-off-by: 0xAurelou <0xaurelou@gmail.com>

---
+++ test/lib/forks/Ethereum.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier:	AGPL-3.0
+pragma solidity ^0.8.10;
+
+import {GenericFork} from "./Generic.sol";
+
+contract EthereumFork is GenericFork {
+  constructor() {
+    CHAIN_ID = 1;
+    NAME = "ethereum";
+    NETWORK = "mainnet";
+  }
+}

e7fca885f325f4b287b9d0ccc11dfff51602d958
Espen Højsgaard <espen@espen.dk>
2023-05-09 14:02:47 +0200

    Replace Mangrove deployment Solidity script for Mumbai with new controlled deployment script

---
+++ deployment/scripts/mumbai/run-MangroveDeployer-mumbai.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+DEPLOYMENT_LIB_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../../lib" &> /dev/null && pwd )
+
+"${DEPLOYMENT_LIB_DIR}/controlledEnvDeploy.sh" mumbai MangroveDeployer CHIEF=0x751a02217777b4B85848169A52d6b035D7Cf5DDd GASPRICE=50 GASMAX=1000000 FOUNDRY_OPTIMIZER=true FOUNDRY_OPTIMIZER_RUNS=20000

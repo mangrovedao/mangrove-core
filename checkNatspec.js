56a4cfab860fb4c24871964f96d6912660902821
Peter <105609241+peterMangrove@users.noreply.github.com>
2023-08-15 13:19:46 +0200

    remove strats (#425)
    * remove strats
    
    * Clean up addresses
    
    * Reinstate faucet scripts and TestToken.t.tsol
    
    * Reinstance ERC20 deployers
    
    * Update checkNastspec
    
    * Move deployers back to toy folder

--- checkNatspec.js
+++ checkNatspec.js
@@ -27,37 +27,7 @@ const read_artifact = (file_path) => {
 // gather all artifact files
 const artifacts = all_files(path.join(cwd, "out"));
 
-includes = [
-  "AbstractKandelSeeder",
-  "AaveKandelSeeder",
-  "KandelSeeder",
-  "Direct",
-  "DirectWithBidsAndAsksDistribution",
-  "HasIndexedBidsAndAsks",
-  "AbstractKandel",
-  "CoreKandel",
-  "TradesBaseQuotePair",
-  "GeometricKandel",
-  "AaveKandel",
-  "Kandel",
-  "AaveV3Lender",
-  "HasAaveBalanceMemoizer",
-  "AbstractRouter",
-  "AavePooledRouter",
-  "MangroveOrder",
-  "IOrderLogic",
-  "Forwarder",
-  "AccessControlled",
-  "AbstractRouter",
-  "SimpleRouter",
-  "MangroveOffer",
-  "IOfferLogic",
-  "IForwarder",
-  "TransferLib",
-  "ILiquidityProvider",
-  "OfferMakerTutorial",
-  "OfferMakerTutorialResidual",
-];
+includes = ["TransferLib"];
 
 let anyFindings = false;
 artifacts.forEach((file) => {

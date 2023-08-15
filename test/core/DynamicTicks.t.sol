c07a33148b1749d2319f5aafbd0cfeafd04e7dfb
Adrien Husson <adhusson@gmail.com>
2023-08-15 23:29:25 +0200

    Update test/core/DynamicTicks.t.sol
    Co-authored-by: Lasse <16150076+lnist@users.noreply.github.com>

--- test/core/DynamicTicks.t.sol
+++ test/core/DynamicTicks.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.10;
 import "mgv_test/lib/MangroveTest.sol";
 import {MgvStructs, MAX_TICK, MIN_TICK, LogPriceLib} from "mgv_src/MgvLib.sol";
 import {DensityLib} from "mgv_lib/DensityLib.sol";
-import {stdError} from "forge-std/stdError.sol";
+import {stdError} from "forge-std/StdError.sol";
 
 // In these tests, the testing contract is the market maker.
 contract DynamicTicksTest is MangroveTest {

9035706723c5c8914de8762d4e0fc76591f49a7f
Espen Højsgaard <espen@espen.dk>
2023-08-28 09:40:05 +0200

    cleanup + move test offer data to struct

--- test/core/ticktree/TickTreeNewOffer.t.sol
+++ test/core/ticktree/TickTreeNewOffer.t.sol
@@ -45,39 +45,13 @@ import {
 } from "mgv_src/MgvLib.sol";
 import "mgv_lib/Debug.sol";
 
-// import {TickLib, Tick, MIN_TICK, MAX_TICK, LEAF_SIZE, LEVEL0_SIZE, LEVEL1_SIZE, LEVEL2_SIZE, NUM_LEVEL1, NUM_LEVEL0, NUM_LEAFS, NUM_TICKS} from "mgv_lib/TickLib.sol";
-
 // FIXME: Extract common constants and utility methods to a test base class
 contract TickTreeNewOfferTest is MangroveTest {
-  function test_constants_are_at_expected_locations() public {
-    // FIXME: Assert that the constants have the expected locations at various levels
-    // This also serves as a reading guide to the constants.
-    // MIN_TICK
-    Tick tick = Tick.wrap(MIN_TICK);
-    // FIXME: This failed because MIN_TICK was 1 bigger than the smallest number representable by int20. I've changed MIN_TICK the min value of int20.
-    assertEq(tick.posInLeaf(), 0);
-    assertEq(tick.posInLevel0(), 0);
-    assertEq(tick.posInLevel1(), 0);
-    assertEq(tick.posInLevel2(), 0);
+  function setUp() public override {
+    super.setUp();
 
-    // MAX_TICK
-    tick = Tick.wrap(MAX_TICK);
-    assertEq(tick.posInLeaf(), uint(LEAF_SIZE - 1));
-    assertEq(tick.posInLevel0(), uint(LEVEL0_SIZE - 1));
-    assertEq(tick.posInLevel1(), uint(LEVEL1_SIZE - 1));
-    assertEq(tick.posInLevel2(), uint(LEVEL2_SIZE - 1));
-  }
-
-  //FIXME: Price calculations currently don't work for MIN_TICK and it is therefore impossible to post offers at MIN_TICK
-  function testFail_new_offer_in_empty_tree_at_min_tick() public {
-    Tick tick = Tick.wrap(MIN_TICK);
-    int logPrice = LogPriceLib.fromTick(tick, olKey.tickScale);
-    uint gasreq = 100_000;
-    uint gives = reader.minVolume(olKey, gasreq);
-
-    uint ofr = mgv.newOfferByLogPrice(olKey, logPrice, gives, gasreq, 0);
-    assertEq(mgv.best(olKey), ofr);
-    //FIXME: Complete this test
+    // Check that the tick tree is consistent after set up
+    assertTickTreeIsConsistent();
   }
 
   function assertTickAssumptions(
@@ -97,6 +71,51 @@ contract TickTreeNewOfferTest is MangroveTest {
     assertEq(tick.posInLevel1(), posInLevel1, "tick's posInLevel1 does not match expected value");
     assertEq(tick.level1Index(), level1Index, "tick's level1Index does not match expected value");
     assertEq(tick.posInLevel2(), posInLevel2, "tick's posInLevel2 does not match expected value");
+  }
+
+  // Cheks that the ticks used in these tests have the expected locations at various levels.
+  // This also serves as a reading guide to the constants.
+  function test_ticks_are_at_expected_locations() public {
+    assertTickAssumptions({
+      tick: Tick.wrap(MIN_TICK),
+      posInLeaf: 0,
+      leafIndex: MIN_LEAF_INDEX,
+      posInLevel0: 0,
+      level0Index: MIN_LEVEL0_INDEX,
+      posInLevel1: 0,
+      level1Index: MIN_LEVEL1_INDEX,
+      posInLevel2: 0
+    });
+
+    assertTickAssumptions({
+      tick: Tick.wrap(MAX_TICK),
+      // FIXME: This failed because MIN_TICK was 1 bigger than the smallest number representable by int20. I've changed MIN_TICK the min value of int20.
+      posInLeaf: MAX_LEAF_POSITION,
+      leafIndex: MAX_LEAF_INDEX,
+      posInLevel0: MAX_LEVEL0_POSITION,
+      level0Index: MAX_LEVEL0_INDEX,
+      posInLevel1: MAX_LEVEL1_POSITION,
+      level1Index: MAX_LEVEL1_INDEX,
+      posInLevel2: MAX_LEVEL2_POSITION
+    });
+  }
+
+  struct OfferData {
+    uint id;
+    Tick tick;
+    int logPrice;
+    uint gives;
+    uint gasreq;
+    uint gasprice;
+  }
+
+  function createOfferData(Tick tick, uint gasreq, uint gasprice) internal returns (OfferData memory offerData) {
+    offerData.id = 0;
+    offerData.tick = tick;
+    offerData.logPrice = LogPriceLib.fromTick(tick, olKey.tickScale);
+    offerData.gasreq = gasreq;
+    offerData.gives = reader.minVolume(olKey, offerData.gasreq);
+    offerData.gasprice = gasprice;
   }
 
   function setBit(Field field, uint pos) internal pure returns (Field) {
@@ -179,35 +198,37 @@ contract TickTreeNewOfferTest is MangroveTest {
   // Checks that the given offer has been correctly added to `offerBranchBefore`
   function assertOfferAddedCorrectlyToBranch(
     TickTreeBranch memory offerBranchBefore,
-    uint offerId,
-    Tick tick,
-    int logPrice
+    OfferData memory expectedOfferData
   ) internal {
-    MgvStructs.OfferPacked offer = mgv.offers(olKey, offerId);
-    assertEq(offer.logPrice(), logPrice, "offer's logPrice does not match insertion logPrice");
-    assertEq(offer.tick(olKey.tickScale), tick, "offer's logPrice does not match insertion logPrice");
+    MgvStructs.OfferPacked offer = mgv.offers(olKey, expectedOfferData.id);
+    assertEq(offer.logPrice(), expectedOfferData.logPrice, "offer's logPrice does not match insertion logPrice");
+    assertEq(offer.tick(olKey.tickScale), expectedOfferData.tick, "offer's logPrice does not match insertion logPrice");
 
     // Check that the offer is added correctly to the tick list
-    uint tickFirstBefore = offerBranchBefore.leaf.firstOfIndex(tick.posInLeaf());
-    uint tickLastBefore = offerBranchBefore.leaf.firstOfIndex(tick.posInLeaf());
+    uint tickFirstBefore = offerBranchBefore.leaf.firstOfIndex(expectedOfferData.tick.posInLeaf());
+    uint tickLastBefore = offerBranchBefore.leaf.firstOfIndex(expectedOfferData.tick.posInLeaf());
     assertEq(offer.prev(), tickLastBefore, "offer.prev should be old last in tick list");
     assertEq(offer.next(), 0, "offer.next should be empty");
     if (tickLastBefore != 0) {
       MgvStructs.OfferPacked prevOffer = mgv.offers(olKey, tickLastBefore);
-      assertEq(prevOffer.next(), offerId, "prevOffer.next should be new offer");
+      assertEq(prevOffer.next(), expectedOfferData.id, "prevOffer.next should be new offer");
     }
 
     // Check that the leaf is updated correctly
-    Leaf leaf = mgv.leafs(olKey, tick.leafIndex());
+    Leaf leaf = mgv.leafs(olKey, expectedOfferData.tick.leafIndex());
     assertEq(
-      leaf.firstOfIndex(tick.posInLeaf()),
-      tickFirstBefore == 0 ? offerId : tickFirstBefore,
+      leaf.firstOfIndex(expectedOfferData.tick.posInLeaf()),
+      tickFirstBefore == 0 ? expectedOfferData.id : tickFirstBefore,
       "offer should only be first in the tick list if it was empty before"
     );
-    assertEq(leaf.lastOfIndex(tick.posInLeaf()), offerId, "offer should be last in the tick list");
+    assertEq(
+      leaf.lastOfIndex(expectedOfferData.tick.posInLeaf()),
+      expectedOfferData.id,
+      "offer should be last in the tick list"
+    );
     // Rest of leaf should be unchanged
     for (uint i = 0; i < uint(LEAF_SIZE); ++i) {
-      if (i == tick.posInLeaf()) {
+      if (i == expectedOfferData.tick.posInLeaf()) {
         continue;
       }
       assertEq(leaf.firstOfIndex(i), offerBranchBefore.leaf.firstOfIndex(i), "other leaf positions should be unchanged");
@@ -216,23 +237,23 @@ contract TickTreeNewOfferTest is MangroveTest {
 
     // Check that the levels are updated correctly
     assertEq(
-      mgv.level0(olKey, tick.level0Index()),
-      setBit(offerBranchBefore.level0, tick.posInLevel0()),
+      mgv.level0(olKey, expectedOfferData.tick.level0Index()),
+      setBit(offerBranchBefore.level0, expectedOfferData.tick.posInLevel0()),
       "level0 should have bit set for tick"
     );
-    Field level1 = mgv.level1(olKey, tick.level1Index());
-    assertEq(level1, setBit(level1, tick.posInLevel1()), "level1 should have bit set for tick");
+    Field level1 = mgv.level1(olKey, expectedOfferData.tick.level1Index());
+    assertEq(level1, setBit(level1, expectedOfferData.tick.posInLevel1()), "level1 should have bit set for tick");
     Field level2 = mgv.level2(olKey);
-    assertEq(level2, setBit(level2, tick.posInLevel2()), "level2 should have bit set for tick");
+    assertEq(level2, setBit(level2, expectedOfferData.tick.posInLevel2()), "level2 should have bit set for tick");
   }
 
   // Checks that the best tick branch has been updated correctly to the new best offer
   function assertBestBranchUpdatedCorrectly(
     TickTreeBranch memory bestBranchBefore,
     TickTreeBranch memory offerBranchBefore,
-    uint bestOfferId
+    OfferData memory expectedBestOfferData
   ) internal {
-    MgvStructs.OfferPacked bestOffer = mgv.offers(olKey, bestOfferId);
+    MgvStructs.OfferPacked bestOffer = mgv.offers(olKey, expectedBestOfferData.id);
     Tick tick = bestOffer.tick(olKey.tickScale);
 
     // Check that local contains the offer's tick branch and that Mangrove reports it correctly
@@ -269,7 +290,7 @@ contract TickTreeNewOfferTest is MangroveTest {
         "mgv.level0 should have bit set for best offer's tick branch"
       );
       assertEq(local.tickPosInLeaf(), tick.posInLeaf(), "local.tickPosInLeaf should be best offer's position in leaf");
-      assertEq(mgv.best(olKey), bestOfferId, "offerId should be best");
+      assertEq(mgv.best(olKey), expectedBestOfferData.id, "offerId should be best");
     }
 
     // Check that any part of the previous best branch that is no longer part of the best branch is unchanged and has been written to the mappings
@@ -339,53 +360,72 @@ contract TickTreeNewOfferTest is MangroveTest {
     }
   }
 
-  function assertOfferRemovedCorrectlyFromBranch(TickTreeBranch memory offerBranchBefore, uint offerId, Tick tick)
+  function assertOfferUpdatedCorrectlyOnBranch(
+    TickTreeBranch memory oldOfferBranchBefore,
+    TickTreeBranch memory offerBranchBefore,
+    OfferData memory expectedOfferData
+  ) internal {
+    MgvStructs.OfferPacked offer = mgv.offers(olKey, expectedOfferData.id);
+    assertTrue(offer.isLive(), "should be live after update");
+    // FIXME: Implement this
+
+    // FIXME: If offer was best, check that the best branch is updated correctly
+  }
+
+  function assertOfferRemovedCorrectlyFromBranch(TickTreeBranch memory offerBranchBefore, OfferData memory offerData)
     internal
   {
-    MgvStructs.OfferPacked offer = mgv.offers(olKey, offerId);
+    MgvStructs.OfferPacked offer = mgv.offers(olKey, offerData.id);
     assertFalse(offer.isLive(), "should not be live after retract");
 
-    // TODO:
-    // - If the tick list is now empty, check that the branch is removed correctly from the levels
-    // - If the tick list is not empty, check that the branch is still active
-    uint offerIdBefore = offerBranchBefore.leaf.firstOfIndex(tick.posInLeaf());
+    uint offerIdBefore = offerBranchBefore.leaf.firstOfIndex(offerData.tick.posInLeaf());
     assertNotEq(offerIdBefore, 0, "tick list should not have been empty before offer was retracted");
-    Leaf leaf = mgv.leafs(olKey, tick.leafIndex());
-    uint offerIdInTickList = leaf.firstOfIndex(tick.posInLeaf());
+    Leaf leaf = mgv.leafs(olKey, offerData.tick.leafIndex());
+    uint offerIdInTickList = leaf.firstOfIndex(offerData.tick.posInLeaf());
     bool tickListIsEmpty = offerIdInTickList == 0;
 
     while (offerIdInTickList != 0) {
-      assertNotEq(offerIdInTickList, offerId, "offer should have been removed from tick list");
+      assertNotEq(offerIdInTickList, offerData.id, "offer should have been removed from tick list");
       MgvStructs.OfferPacked offerInTickList = mgv.offers(olKey, offerIdInTickList);
       offerIdInTickList = offerInTickList.next();
     }
 
     if (tickListIsEmpty) {
-      assertEq(leaf.lastOfIndex(tick.posInLeaf()), 0, "last in tick list should be empty");
+      assertEq(leaf.lastOfIndex(offerData.tick.posInLeaf()), 0, "last in tick list should be empty");
       if (leaf.eq(LeafLib.EMPTY)) {
-        Field level0 = mgv.level0(olKey, tick.level0Index());
-        assertEq(level0, unsetBit(offerBranchBefore.level0, tick.posInLevel0()), "level0 pos should be unset for tick");
+        Field level0 = mgv.level0(olKey, offerData.tick.level0Index());
+        assertEq(
+          level0,
+          unsetBit(offerBranchBefore.level0, offerData.tick.posInLevel0()),
+          "level0 pos should be unset for tick"
+        );
         if (level0.eq(FieldLib.EMPTY)) {
-          Field level1 = mgv.level1(olKey, tick.level1Index());
+          Field level1 = mgv.level1(olKey, offerData.tick.level1Index());
           assertEq(
-            level1, unsetBit(offerBranchBefore.level1, tick.posInLevel1()), "level1 pos should be unset for tick"
+            level1,
+            unsetBit(offerBranchBefore.level1, offerData.tick.posInLevel1()),
+            "level1 pos should be unset for tick"
           );
           if (level1.eq(FieldLib.EMPTY)) {
             Field level2 = mgv.level2(olKey);
             assertEq(
-              level2, unsetBit(offerBranchBefore.level2, tick.posInLevel2()), "level2 pos should be unset for tick"
+              level2,
+              unsetBit(offerBranchBefore.level2, offerData.tick.posInLevel2()),
+              "level2 pos should be unset for tick"
             );
           }
         }
       }
     } else {
       assertTrue(
-        isBitSet(mgv.level0(olKey, tick.level0Index()), tick.posInLevel0()), "level0 pos should be set for tick"
+        isBitSet(mgv.level0(olKey, offerData.tick.level0Index()), offerData.tick.posInLevel0()),
+        "level0 pos should be set for tick"
       );
       assertTrue(
-        isBitSet(mgv.level1(olKey, tick.level1Index()), tick.posInLevel1()), "level1 pos should be set for tick"
+        isBitSet(mgv.level1(olKey, offerData.tick.level1Index()), offerData.tick.posInLevel1()),
+        "level1 pos should be set for tick"
       );
-      assertTrue(isBitSet(mgv.level2(olKey), tick.posInLevel2()), "level2 pos should be set for tick");
+      assertTrue(isBitSet(mgv.level2(olKey), offerData.tick.posInLevel2()), "level2 pos should be set for tick");
     }
 
     // FIXME: If offer was best, check that the best branch is updated correctly
@@ -416,66 +456,84 @@ contract TickTreeNewOfferTest is MangroveTest {
     return snapshotTickTreeBranch(tick);
   }
 
-  function test_new_offer_in_empty_tree_at_max_tick() public {
-    Tick tick = Tick.wrap(MAX_TICK);
+  //FIXME: Price calculations currently don't work for MIN_TICK and it is therefore impossible to post offers at MIN_TICK
+  function testFail_new_offer_in_empty_tree_at_min_tick() public {
+    Tick tick = Tick.wrap(MIN_TICK);
     int logPrice = LogPriceLib.fromTick(tick, olKey.tickScale);
     uint gasreq = 100_000;
     uint gives = reader.minVolume(olKey, gasreq);
 
-    // Tick assumptions
-    // FIXME: Is this still needed? Do we use the constants?
-    // assertTickAssumptions({
-    //   tick: tick,
-    //   posInLeaf: MAX_LEAF_POSITION,
-    //   leafIndex: MAX_LEAF_INDEX,
-    //   posInLevel0: MAX_LEVEL0_POSITION,
-    //   level0Index: MAX_LEVEL0_INDEX,
-    //   posInLevel1: MAX_LEVEL1_POSITION,
-    //   level1Index: MAX_LEVEL1_INDEX,
-    //   posInLevel2: MAX_LEVEL2_POSITION
-    // });
+    uint ofr = mgv.newOfferByLogPrice(olKey, logPrice, gives, gasreq, 0);
+    assertEq(mgv.best(olKey), ofr);
+    //FIXME: Complete this test
+  }
 
-    TickTreeBranch memory offerBranchBefore = snapshotTickTreeBranch(tick);
+  function test_new_offer_in_empty_tree_at_max_tick() public {
+    OfferData memory offerData1 = createOfferData(Tick.wrap(MAX_TICK), 100_000, 1);
+
+    TickTreeBranch memory offerBranchBefore = snapshotTickTreeBranch(offerData1.tick);
     TickTreeBranch memory bestBranchBefore = snapshotBestTickTreeBranch();
     // TODO:
     // Assert that the tick tree is empty. Maybe do this in a separate test, verifying that setup acts as expected
 
-    // logTickTreeBranch(reader, olKey);
-    assertTickTreeIsConsistent();
-    uint ofr = mgv.newOfferByLogPrice(olKey, logPrice, gives, gasreq, 0);
+    offerData1.id =
+      mgv.newOfferByLogPrice(olKey, offerData1.logPrice, offerData1.gives, offerData1.gasreq, offerData1.gasprice);
     // logTickTreeBranch(reader, olKey);
     assertTickTreeIsConsistent();
     // TODO:
     // how to check that other leafs and levels are empty?
 
     // Lasse: Maybe these can be combined into one, clearer function
-    assertOfferAddedCorrectlyToBranch(offerBranchBefore, ofr, tick, logPrice);
-    assertBestBranchUpdatedCorrectly(bestBranchBefore, offerBranchBefore, ofr);
+    assertOfferAddedCorrectlyToBranch(offerBranchBefore, offerData1);
+    assertBestBranchUpdatedCorrectly(bestBranchBefore, offerBranchBefore, offerData1);
   }
 
   function test_retract_only_offer() public {
-    Tick tick = Tick.wrap(MAX_TICK);
-    int logPrice = LogPriceLib.fromTick(tick, olKey.tickScale);
-    uint gasreq = 100_000;
-    uint gives = reader.minVolume(olKey, gasreq);
+    OfferData memory offerData1 = createOfferData(Tick.wrap(MAX_TICK), 100_000, 1);
 
-    TickTreeBranch memory offerBranchBefore = snapshotTickTreeBranch(tick);
+    TickTreeBranch memory offerBranchBefore = snapshotTickTreeBranch(offerData1.tick);
     TickTreeBranch memory bestBranchBefore = snapshotBestTickTreeBranch();
 
-    assertTickTreeIsConsistent();
-    uint ofr = mgv.newOfferByLogPrice(olKey, logPrice, gives, gasreq, 0);
+    offerData1.id =
+      mgv.newOfferByLogPrice(olKey, offerData1.logPrice, offerData1.gives, offerData1.gasreq, offerData1.gasprice);
     assertTickTreeIsConsistent();
 
-    assertOfferAddedCorrectlyToBranch(offerBranchBefore, ofr, tick, logPrice);
-    assertBestBranchUpdatedCorrectly(bestBranchBefore, offerBranchBefore, ofr);
+    assertOfferAddedCorrectlyToBranch(offerBranchBefore, offerData1);
+    assertBestBranchUpdatedCorrectly(bestBranchBefore, offerBranchBefore, offerData1);
 
-    offerBranchBefore = snapshotTickTreeBranch(tick);
+    offerBranchBefore = snapshotTickTreeBranch(offerData1.tick);
     bestBranchBefore = snapshotBestTickTreeBranch();
 
-    mgv.retractOffer(olKey, ofr, false);
+    mgv.retractOffer(olKey, offerData1.id, false);
     assertTickTreeIsConsistent();
 
-    assertOfferRemovedCorrectlyFromBranch(offerBranchBefore, ofr, tick);
+    assertOfferRemovedCorrectlyFromBranch(offerBranchBefore, offerData1);
+  }
+
+  function test_update_only_offer() public {
+    OfferData memory offerData1 = createOfferData(Tick.wrap(MAX_TICK), 100_000, 1);
+    OfferData memory offerData2 = createOfferData(Tick.wrap(MAX_TICK - 1), 200_000, 2);
+    TickTreeBranch memory offerBranchBefore = snapshotTickTreeBranch(offerData1.tick);
+    TickTreeBranch memory bestBranchBefore = snapshotBestTickTreeBranch();
+
+    offerData1.id =
+      mgv.newOfferByLogPrice(olKey, offerData1.logPrice, offerData1.gives, offerData1.gasreq, offerData1.gasprice);
+    assertTickTreeIsConsistent();
+
+    assertOfferAddedCorrectlyToBranch(offerBranchBefore, offerData1);
+    assertBestBranchUpdatedCorrectly(bestBranchBefore, offerBranchBefore, offerData1);
+
+    TickTreeBranch memory oldOfferBranchBefore = snapshotTickTreeBranch(offerData1.tick);
+    offerBranchBefore = snapshotTickTreeBranch(offerData2.tick);
+    bestBranchBefore = snapshotBestTickTreeBranch();
+
+    offerData2.id = offerData1.id;
+    mgv.updateOfferByLogPrice(
+      olKey, offerData2.logPrice, offerData2.gives, offerData2.gasreq, offerData2.gasprice, offerData2.id
+    );
+    assertTickTreeIsConsistent();
+
+    assertOfferUpdatedCorrectlyOnBranch(oldOfferBranchBefore, offerBranchBefore, offerData2);
   }
 
   // Lasse: Can we add these assertions to existing tests? Eg by adding a wrapper to Mangrove and adding assertions to that

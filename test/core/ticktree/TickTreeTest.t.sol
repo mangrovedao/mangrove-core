40e167be1bf3243c1c09add19ebc8f877e9a9208
Espen Højsgaard <espen@espen.dk>
2023-08-29 20:30:17 +0200

    re-enable MAX_TICK for higher tick in scenarios

--- test/core/ticktree/TickTreeTest.t.sol
+++ test/core/ticktree/TickTreeTest.t.sol
@@ -485,10 +485,9 @@ abstract contract TickTreeTest is MangroveTest {
     if (_tick.posInLevel2() < MAX_LEVEL2_POSITION) {
       ticks[next++] = tick + LEAF_SIZE * LEVEL0_SIZE * LEVEL1_SIZE; // in level2
     }
-    // FIXME: MAX_TICK is currently out of range
-    // if (tick < MAX_TICK) {
-    //   ticks[next++] = MAX_TICK; // at max tick
-    // }
+    if (tick < MAX_TICK) {
+      ticks[next++] = MAX_TICK; // at max tick
+    }
 
     int[] memory res = new int[](next);
     for (uint i = 0; i < next; ++i) {

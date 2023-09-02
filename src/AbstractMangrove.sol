42656ac1b19be887e35bf7c8761b19fa50eec822
Adrien Husson <adhusson@gmail.com>
2023-09-02 23:52:15 +0200

    refactor: simplify contract structure

--- src/AbstractMangrove.sol
+++ src/AbstractMangrove.sol
@@ -5,12 +5,41 @@ import {MgvLib} from "./MgvLib.sol";
 
 import {MgvOfferMaking} from "./MgvOfferMaking.sol";
 import {MgvOfferTakingWithPermit} from "./MgvOfferTakingWithPermit.sol";
-import {MgvHasAppendix} from "./MgvHasAppendix.sol";
+import {MgvAppendix} from "mgv_src/MgvAppendix.sol";
 
-/* `AbstractMangrove` inherits the three contracts that implement generic Mangrove functionality (`MgvGovernable`,`MgvOfferTakingWithPermit` and `MgvOfferMaking`) but does not implement the abstract functions. */
-abstract contract AbstractMangrove is MgvHasAppendix, MgvOfferTakingWithPermit, MgvOfferMaking {
-  constructor(address governance, uint gasprice, uint gasmax, string memory contractName)
+/* `AbstractMangrove` inherits the three contracts that implement generic Mangrove functionality (`MgvOfferTakingWithPermit` and `MgvOfferMaking`) but does not implement the abstract functions. */
+abstract contract AbstractMangrove is MgvOfferTakingWithPermit, MgvOfferMaking {
+  address immutable appendix;
+
+  constructor(address _governance, uint _gasprice, uint gasmax, string memory contractName)
     MgvOfferTakingWithPermit(contractName)
-    MgvHasAppendix(governance, gasprice, gasmax)
-  {}
+  {
+    unchecked {
+      emit NewMgv();
+
+      appendix = address(new MgvAppendix());
+
+      /* Initially, governance is open to anyone. */
+      /* Set initial gasprice and gasmax. */
+      bool success;
+      (success,) = appendix.delegatecall(abi.encodeCall(MgvAppendix.setGasprice, (_gasprice)));
+      require(success, "mgv/ctor/gasprice");
+      (success,) = appendix.delegatecall(abi.encodeCall(MgvAppendix.setGasmax, (gasmax)));
+      require(success, "mgv/ctor/gasmax");
+      /* Initialize governance to `_governance` after parameter setting. */
+      (success,) = appendix.delegatecall(abi.encodeCall(MgvAppendix.setGovernance, (_governance)));
+      require(success, "mgv/ctor/governance");
+    }
+  }
+
+  fallback(bytes calldata callData) external returns (bytes memory) {
+    (bool success, bytes memory res) = appendix.delegatecall(callData);
+    if (success) {
+      return res;
+    } else {
+      assembly ("memory-safe") {
+        revert(add(res, 32), mload(res))
+      }
+    }
+  }
 }

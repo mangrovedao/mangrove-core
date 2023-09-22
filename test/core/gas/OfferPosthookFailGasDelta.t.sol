b977eb9a922807b7f3e5f81ab04f08d4b10f7419
lnist <16150076+lnist@users.noreply.github.com>
2023-09-22 16:03:43 +0200

    fix(term): defaultBinScale to defaultTickSpacing

--- test/core/gas/OfferPosthookFailGasDelta.t.sol
+++ test/core/gas/OfferPosthookFailGasDelta.t.sol
@@ -98,8 +98,8 @@ contract OfferPosthookFailGasDeltaTest is MangroveTest, IMaker {
     base = _base;
     quote = _quote;
 
-    olKey = OLKey($(base), $(quote), options.defaultBinScale);
-    lo = OLKey($(quote), $(base), options.defaultBinScale);
+    olKey = OLKey($(base), $(quote), options.defaultTickSpacing);
+    lo = OLKey($(quote), $(base), options.defaultTickSpacing);
 
     setupMarket(olKey);
     setupMarket(lo);

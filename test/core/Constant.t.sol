f161c4a5ec8b3620603888a021ebc357c004dd99
Adrien Husson <adhusson@gmail.com>
2023-09-24 13:46:11 +0200

    literal constants

--- test/core/Constant.t.sol
+++ test/core/Constant.t.sol
@@ -54,4 +54,27 @@ contract ConstantsTest is MangroveTest {
   function test_offer_bits() public {
     assertEq(OFFER_BITS, last_bits);
   }
+
+  // Since constant expressions are (as of solidity 0.8.21) not evaluated at compile time, we write all constants in Constants.sol as literals and test their values here:
+  function test_constant_expressions() public {
+    assertEq(MAX_BIN, -MIN_BIN - 1, "MAX_BIN");
+    assertEq(LEAF_SIZE, int(2 ** LEAF_SIZE_BITS), "LEAF_SIZE");
+    assertEq(LEVEL_SIZE, int(2 ** LEVEL_SIZE_BITS), "LEVEL_SIZE");
+    assertEq(ROOT_SIZE, int(2 ** ROOT_SIZE_BITS), "ROOT_SIZE");
+    assertEq(LEAF_SIZE_MASK, ~(ONES << LEAF_SIZE_BITS), "LEAF_SIZE_MASK");
+    assertEq(LEVEL_SIZE_MASK, ~(ONES << LEVEL_SIZE_BITS), "LEVEL_SIZE_MASK");
+    assertEq(ROOT_SIZE_MASK, ~(ONES << ROOT_SIZE_BITS), "ROOT_SIZE_MASK");
+    assertEq(NUM_LEVEL1, int(ROOT_SIZE), "NUM_lEVEL1");
+    assertEq(NUM_LEVEL2, NUM_LEVEL1 * LEVEL_SIZE, "NUM_LEVEL2");
+    assertEq(NUM_LEVEL3, NUM_LEVEL2 * LEVEL_SIZE, "NUM_LEVEL3");
+    assertEq(NUM_LEAFS, NUM_LEVEL3 * LEVEL_SIZE, "NUM_LEAFS");
+    assertEq(NUM_BINS, NUM_LEAFS * LEAF_SIZE, "NUM_BINS");
+    assertEq(OFFER_MASK, ONES >> (256 - OFFER_BITS), "OFFER_MASK");
+    assertEq(MIN_TICK, -((1 << 20) - 1), "MIN_TICK");
+    assertEq(MAX_TICK, -MIN_TICK, "MAX_TICK");
+    assertEq(MANTISSA_BITS_MINUS_ONE, MANTISSA_BITS - 1, "MANTISSA_BITS_MINUS_ONE");
+    assertEq(MAX_SAFE_VOLUME, (1 << (256 - MANTISSA_BITS)) - 1, "MAX_SAFE_VOLUME");
+    assertEq(MIN_BIN_ALLOWED, MIN_TICK, "MIN_BIN_ALLOWED");
+    assertEq(MAX_BIN_ALLOWED, MAX_TICK, "MAX_BIN_ALLOWED");
+  }
 }

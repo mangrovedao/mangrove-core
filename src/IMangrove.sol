11784994d2bedf9728acac2262b5ea2c74f65c2a
Lasse <16150076+lnist@users.noreply.github.com>
2023-10-02 20:17:44 +0200

    Update src/IMangrove.sol
    Co-authored-by: Espen Højsgaard <espen@mangrove.exchange>

--- src/IMangrove.sol
+++ src/IMangrove.sol
@@ -294,7 +294,7 @@ interface IMangrove is HasMgvEvents {
   ///@notice Gets an offer detail in packed format.
   ///@param olKey The offer list key given by (maker) `outbound_tkn`, (maker) `inbound_tkn`, and `tickSpacing`.
   ///@param offerId The `offerId` of the offer on the offer list.
-  ///@return offerDetail The offer detail in packed format.
+  ///@return offerDetail The offer details in packed format.
   function offerDetails(OLKey memory olKey, uint offerId) external view returns (OfferDetail offerDetail);
 
   ///@notice Gets both an offer and an offer detail in packed format.

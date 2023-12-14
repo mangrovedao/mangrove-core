2d116ebb62d3967c11c9f72ec4bd3f060e691ac0
Espen Højsgaard <espen@espen.dk>
2023-12-14 11:29:12 +0100

    docs(license): Consistently use AGPL-3.0-or-later for tools, scripts, and tests

--- preprocessing/StructTest.pre.sol.ts
+++ preprocessing/StructTest.pre.sol.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 import { format, tabulate } from "./lib/format";
 
 export const template = ({ preamble, struct_utilities, struct: s }) => {
--- preprocessing/Structs.pre.sol.ts
+++ preprocessing/Structs.pre.sol.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 import { format } from "./lib/format";
 
 export const template = ({ preamble, structs }) => {
